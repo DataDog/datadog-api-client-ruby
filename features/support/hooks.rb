eeaaeaa392d1f1ef9691ada8e9a6ff5436e891d3
Jiri Kuncar <jiri.kuncar@gmail.com>
2020-09-28 13:56:53 +0200

    Use AfterConfiguration events

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,32 +1,35 @@
-Around do |scenario, block|
-  Datadog.tracer.trace('test', { 'resource': scenario.name, 'span_type': 'test' }) do |span|
+AfterConfiguration do |config|
+  config.on_event :test_case_started do |event|
+    span = Datadog.tracer.trace('test', { 'resource': event.test_case.name, 'span_type': 'test' })
     span.set_tag(Datadog::Ext::Analytics::TAG_ENABLED, true)
-    scenario.tags.each do |tag|
+    event.test_case.tags.each do |tag|
       prefix = '@endpoint('
       span.set_tag('version', tag.name[prefix.length...-1]) if tag.name.start_with? prefix
     end
-    block.call
   end
-end
 
-AfterConfiguration do |config|
+  config.on_event :test_case_finished do |event|
+    current_span = Datadog.tracer.active_span
+    unless current_span.nil?
+      if current_span.span_type == 'test'
+        if event.result.failed?
+          current_span.set_tag(Datadog::Ext::Errors::MSG, "failed")
+        end
+        current_span.finish
+      end
+    end
+  end
+
   config.on_event :test_step_started do |step|
     Datadog.tracer.trace('step', { 'resource': step.test_step.to_s, 'span_type': 'step' })
   end
-end
 
-AfterConfiguration do |config|
   config.on_event :test_step_finished do |step|
     current_span = Datadog.tracer.active_span
     unless current_span.nil?
       if current_span.span_type == 'step'
         unless step.result.passed?
           current_span.set_error step.result.exception
-          # mark all parents spans as failed
-          s = current_span
-          while s = s.parent
-            s.set_tag(Datadog::Ext::Errors::MSG, "failed #{step.test_step}") unless s.get_tag(Datadog::Ext::Errors::MSG)
-          end
         end
         current_span.finish
       end
