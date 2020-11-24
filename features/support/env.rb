950e380a83c51bdb9c75c8dfd0d9568b13ef495f
Hippolyte HENRY <zippolyte@users.noreply.github.com>
2020-11-24 16:44:29 +0100

    Update features/support/env.rb
    Co-authored-by: Jiri Kuncar <jiri.kuncar@datadoghq.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -13,7 +13,7 @@ Datadog.configure do |c|
   c.analytics_enabled = true
   c.use :cucumber, {'operation_name': 'test'}
   c.use :ethon, {}
-  c.diagnostics.debug = (!ENV["DEBUG"].nil? and ENV["DEBUG"] != "false")
+  c.diagnostics.debug = ENV["DEBUG"].to_s.downcase != "false"
 end
 
 module RecordMode
