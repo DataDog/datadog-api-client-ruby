d8e41aecf7486a60881a8dd95a34f582dd68821e
Thomas Hervé <thomas.herve@datadoghq.com>
2020-12-10 08:48:55 +0100

    Call bundle install for rubocop to work

--- check-examples.sh
+++ check-examples.sh
@@ -2,6 +2,7 @@
 
 ./extract-code-blocks.sh
 
+bundle install
 ls examples/v*/*/*.rbbeta | xargs -P $(($(nproc)*2)) -n 1 bundle exec ruby -cw && bundle exec rubocop examples/v*/*/*.rbbeta
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
