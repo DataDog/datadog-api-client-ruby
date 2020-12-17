3d1e1bfb2db977808195b05c73759054f61eb515
ci.datadog-api-spec <packages@datadoghq.com>
2020-12-17 09:37:27 +0000

    Regenerate client from commit a1a105a of spec repo

--- lib/datadog_api_client/v2.rb
+++ lib/datadog_api_client/v2.rb
@@ -15,9 +15,9 @@ OpenAPI Generator version: 5.0.0-SNAPSHOT
 =end
 
 # Common files
+require 'datadog_api_client/version'
 require 'datadog_api_client/v2/api_client'
 require 'datadog_api_client/v2/api_error'
-require 'datadog_api_client/v2/version'
 require 'datadog_api_client/v2/configuration'
 
 # Models
