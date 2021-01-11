b7a5d072980c5733450e5aaa62ac4e202c617926
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2021-01-11 10:18:49 +0100

    Always run integration-only scenarios

--- run-tests.sh
+++ run-tests.sh
@@ -19,4 +19,11 @@ if [ "$RERECORD_FAILED_TESTS" == "true" -a "$CUCUMBER_RESULT" -ne 0 ]; then
     RECORD=true bundle exec cucumber @rerun.txt
     CUCUMBER_RESULT=$?
 fi
+
+# Always run integratin-only scenarios
+set -e
+if [ "$RECORD" != "none" ]; then
+    RECORD=none bundle exec cucumber -t '@integration-only' -f pretty
+fi
+
 exit $(($RSPEC_RESULT+$CUCUMBER_RESULT))
