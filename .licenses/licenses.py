26fcb5087bea7dbe9a81865b345072c0f80a51bf
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-03-10 10:49:04 +0100

    generate LICENSE-3rdparty.csv

---
+++ .licenses/licenses.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import csv
+import pathlib
+import re
+import yaml
+
+RE_COPYRIGHT = re.compile("Copyright [^ ]+ (.*)$", re.MULTILINE)
+
+
+def copyrights(info):
+    for license in info["licenses"]:
+        if license["sources"] != "LICENSE":
+            continue
+        for match in re.findall(RE_COPYRIGHT, license["text"]):
+            yield match.replace(",", "").replace("  ", " ")
+
+
+fieldnames = ["Origin", "License", "Authors"]
+
+with (pathlib.Path(__file__).parent.parent / "LICENSE-3rdparty.csv").open(
+    "w", newline=""
+) as output:
+    writer = csv.DictWriter(output, fieldnames=fieldnames)
+    writer.writeheader()
+
+    for dep in pathlib.Path(__file__).parent.rglob("*.dep.yml"):
+        with dep.open() as fp:
+            info = yaml.safe_load(fp)
+            writer.writerow(
+                {
+                    "Origin": info["name"],
+                    "License": info["license"],
+                    "Authors": " ".join(copyrights(info)),
+                }
+            )
