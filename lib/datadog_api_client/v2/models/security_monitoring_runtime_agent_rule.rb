0b5ffee8838c72d67b480463a2d3b9e85da0fe4a
ci.datadog-api-spec <packages@datadoghq.com>
2021-03-24 07:14:46 +0000

    Regenerate client from commit e5b275c of spec repo

--- lib/datadog_api_client/v2/models/security_monitoring_runtime_agent_rule.rb
+++ lib/datadog_api_client/v2/models/security_monitoring_runtime_agent_rule.rb
@@ -17,37 +17,19 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # Query for matching rule.
-  class SecurityMonitoringRuleQueryCreate
-    attr_accessor :agent_rule
+  # The Agent rule.
+  class SecurityMonitoringRuntimeAgentRule
+    # The Agent rule ID. Must be unique within the rule.
+    attr_accessor :agent_rule_id
 
-    attr_accessor :aggregation
-
-    # Field for which the cardinality is measured. Sent as an array.
-    attr_accessor :distinct_fields
-
-    # Fields to group by.
-    attr_accessor :group_by_fields
-
-    # The target field to aggregate over when using the sum or max aggregations.
-    attr_accessor :metric
-
-    # Name of the query.
-    attr_accessor :name
-
-    # Query to run on logs.
-    attr_accessor :query
+    # A Runtime Security expression determines what activity should be collected by the Datadog Agent. These logical expressions can use predefined operators and attributes. Tags cannot be used in Runtime Security expressions. Instead, allow or deny based on tags under the advanced option.
+    attr_accessor :expression
 
     # Attribute mapping from ruby-style variable name to JSON key.
     def self.attribute_map
       {
-        :'agent_rule' => :'agentRule',
-        :'aggregation' => :'aggregation',
-        :'distinct_fields' => :'distinctFields',
-        :'group_by_fields' => :'groupByFields',
-        :'metric' => :'metric',
-        :'name' => :'name',
-        :'query' => :'query'
+        :'agent_rule_id' => :'agentRuleId',
+        :'expression' => :'expression'
       }
     end
 
@@ -59,13 +41,8 @@ module DatadogAPIClient::V2
     # Attribute type mapping.
     def self.openapi_types
       {
-        :'agent_rule' => :'SecurityMonitoringRuntimeAgentRule',
-        :'aggregation' => :'SecurityMonitoringRuleQueryAggregation',
-        :'distinct_fields' => :'Array<String>',
-        :'group_by_fields' => :'Array<String>',
-        :'metric' => :'String',
-        :'name' => :'String',
-        :'query' => :'String'
+        :'agent_rule_id' => :'String',
+        :'expression' => :'String'
       }
     end
 
@@ -79,47 +56,23 @@ module DatadogAPIClient::V2
     # @param [Hash] attributes Model attributes in the form of hash
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::SecurityMonitoringRuleQueryCreate` initialize method"
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::SecurityMonitoringRuntimeAgentRule` initialize method"
       end
 
       # check to see if the attribute exists and convert string to symbol for hash key
       attributes = attributes.each_with_object({}) { |(k, v), h|
         if (!self.class.attribute_map.key?(k.to_sym))
-          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::SecurityMonitoringRuleQueryCreate`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
+          fail ArgumentError, "`#{k}` is not a valid attribute in `DatadogAPIClient::V2::SecurityMonitoringRuntimeAgentRule`. Please check the name to make sure it's valid. List of attributes: " + self.class.attribute_map.keys.inspect
         end
         h[k.to_sym] = v
       }
 
-      if attributes.key?(:'agent_rule')
-        self.agent_rule = attributes[:'agent_rule']
+      if attributes.key?(:'agent_rule_id')
+        self.agent_rule_id = attributes[:'agent_rule_id']
       end
 
-      if attributes.key?(:'aggregation')
-        self.aggregation = attributes[:'aggregation']
-      end
-
-      if attributes.key?(:'distinct_fields')
-        if (value = attributes[:'distinct_fields']).is_a?(Array)
-          self.distinct_fields = value
-        end
-      end
-
-      if attributes.key?(:'group_by_fields')
-        if (value = attributes[:'group_by_fields']).is_a?(Array)
-          self.group_by_fields = value
-        end
-      end
-
-      if attributes.key?(:'metric')
-        self.metric = attributes[:'metric']
-      end
-
-      if attributes.key?(:'name')
-        self.name = attributes[:'name']
-      end
-
-      if attributes.key?(:'query')
-        self.query = attributes[:'query']
+      if attributes.key?(:'expression')
+        self.expression = attributes[:'expression']
       end
     end
 
@@ -127,17 +80,12 @@ module DatadogAPIClient::V2
     # @return Array for valid properties with the reasons
     def list_invalid_properties
       invalid_properties = Array.new
-      if @query.nil?
-        invalid_properties.push('invalid value for "query", query cannot be nil.')
-      end
-
       invalid_properties
     end
 
     # Check to see if the all the properties in the model are valid
     # @return true if the model is valid
     def valid?
-      return false if @query.nil?
       true
     end
 
@@ -146,13 +94,8 @@ module DatadogAPIClient::V2
     def ==(o)
       return true if self.equal?(o)
       self.class == o.class &&
-          agent_rule == o.agent_rule &&
-          aggregation == o.aggregation &&
-          distinct_fields == o.distinct_fields &&
-          group_by_fields == o.group_by_fields &&
-          metric == o.metric &&
-          name == o.name &&
-          query == o.query
+          agent_rule_id == o.agent_rule_id &&
+          expression == o.expression
     end
 
     # @see the `==` method
@@ -164,7 +107,7 @@ module DatadogAPIClient::V2
     # Calculates hash code according to all attributes.
     # @return [Integer] Hash code
     def hash
-      [agent_rule, aggregation, distinct_fields, group_by_fields, metric, name, query].hash
+      [agent_rule_id, expression].hash
     end
 
     # Builds the object from hash
