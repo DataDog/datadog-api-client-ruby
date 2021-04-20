0712b65f4acd88f16b58a4ba94e7ecd815a5741c
Thomas Hervé <thomas.herve@datadoghq.com>
2021-04-20 12:34:56 +0200

    Also match on query, not uri

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -22,7 +22,7 @@ Around do |scenario, block|
 end
 
 Around do |scenario, block|
-  VCR.use_cassette(scenario.location.file.chomp('.feature') + "/" + scenario.name.gsub(/[^A-Za-z0-9]+/, '-')[0..100], :record_on_error => false, :match_requests_on => [:method, :host, :path, :uri, :body_as_json]) do |cassette|
+  VCR.use_cassette(scenario.location.file.chomp('.feature') + "/" + scenario.name.gsub(/[^A-Za-z0-9]+/, '-')[0..100], :record_on_error => false, :match_requests_on => [:method, :host, :path, :query, :body_as_json]) do |cassette|
     File.delete(cassette.file) if ENV["RECORD"] == "true" && File.exist?(cassette.file)
     Timecop.freeze(use_real_time? ? Time.now : cassette.originally_recorded_at) do
       block.call
