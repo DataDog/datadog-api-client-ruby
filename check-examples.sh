8e0a3968244954d07d09de43e8d34efed4d543e6
Thomas Hervé <thomas.herve@datadoghq.com>
2021-05-04 10:42:42 +0200

    Handle version parameter

--- check-examples.sh
+++ check-examples.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+if [ $# -eq 0 ]; then
+    echo "No arguments supplied"
+    exit 2
+fi
+
 ./extract-code-blocks.sh
 
 bundle install
-ls examples/v*/*/*.rbbeta | xargs -P $(($(nproc)*2)) -n 1 bundle exec ruby -cw && bundle exec rubocop examples/v*/*/*.rbbeta
+ls examples/$1/*/*.rbbeta | xargs -P $(($(nproc)*2)) -n 1 bundle exec ruby -cw && bundle exec rubocop examples/$1/*/*.rbbeta
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
     exit 1
