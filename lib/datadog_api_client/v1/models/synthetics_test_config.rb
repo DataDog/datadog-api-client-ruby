d3806eb5946b0c766d0eca72efa380c866d6035d
ci.datadog-api-spec <packages@datadoghq.com>
2021-05-27 15:03:53 +0000

    Regenerate client from commit 5311d4c of spec repo

--- lib/datadog_api_client/v1/models/synthetics_test_config.rb
+++ lib/datadog_api_client/v1/models/synthetics_test_config.rb
@@ -103,17 +103,12 @@ module DatadogAPIClient::V1
     # @return Array for valid properties with the reasons
     def list_invalid_properties
       invalid_properties = Array.new
-      if @assertions.nil?
-        invalid_properties.push('invalid value for "assertions", assertions cannot be nil.')
-      end
-
       invalid_properties
     end
 
     # Check to see if the all the properties in the model are valid
     # @return true if the model is valid
     def valid?
-      return false if @assertions.nil?
       true
     end
 
