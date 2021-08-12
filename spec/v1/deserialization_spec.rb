1d4e055dab676b8f90e01a0f8234f07b7f504ba3
Sherzod K <sherzod.karimov@datadoghq.com>
2021-08-12 09:24:08 -0400

    apply code review suggestions

--- spec/v1/deserialization_spec.rb
+++ spec/v1/deserialization_spec.rb
@@ -32,8 +32,8 @@ describe 'Deserialization', skip_before: true do
 
       expect(data).to be_a DatadogAPIClient::V1::SyntheticsAPITest
       expect(data.config.assertions.length).to be 3
-      expect(data.config._has_unparsed).to be true
-      expect(data.config.assertions[2].unparsed_object[:operator]).to eq "A non existent operator"
+      expect(data.config._unparsed).to be true
+      expect(data.config.assertions[2].data[:operator]).to eq "A non existent operator"
     end
 
     it 'should deserialize unknown nested enum in list' do
@@ -45,7 +45,7 @@ describe 'Deserialization', skip_before: true do
 
       expect(data).to be_a DatadogAPIClient::V1::SyntheticsAPITest
       expect(data.options.device_ids.length).to be 3
-      expect(data.options._has_unparsed).to be true
+      expect(data.options._unparsed).to be true
       expect(data.options.device_ids[2].to_hash).to eq DatadogAPIClient::V1::SyntheticsDeviceID.build_from_hash("A non existent device ID").to_hash
     end
 
@@ -70,7 +70,7 @@ describe 'Deserialization', skip_before: true do
 
       expect(data).to be_a DatadogAPIClient::V1::SyntheticsAPITest
       expect(data.config.request).to be_a DatadogAPIClient::V1::SyntheticsTestRequest
-      expect(data.config.request._has_unparsed).to be true
+      expect(data.config.request._unparsed).to be true
       expect(data.config.request.timeout).to eq 30
       expect(data.config.request.method.to_hash).to eq "A non existent method"
     end
@@ -92,8 +92,8 @@ describe 'Deserialization', skip_before: true do
     data = @api_instance.create_logs_archive(@body)
 
     expect(data).to be_a DatadogAPIClient::V2::LogsArchive
-    expect(data.data.attributes._has_unparsed).to be true
-    expect(data.data.attributes.destination.unparsed_object[:type]).to eq "A non existent destination"
+    expect(data.data.attributes._unparsed).to be true
+    expect(data.data.attributes.destination.data[:type]).to eq "A non existent destination"
     end
   end
 end
