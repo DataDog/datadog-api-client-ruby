9df7a7d229cbec0600920e9d2d12252d81155d2e
ci.datadog-api-spec <packages@datadoghq.com>
2021-09-07 16:47:13 +0000

    Regenerate client from commit 8d89f84 of spec repo

--- lib/datadog_api_client/v1/api/organizations_api.rb
+++ lib/datadog_api_client/v1/api/organizations_api.rb
@@ -23,7 +23,7 @@ module DatadogAPIClient::V1
       @api_client = api_client
     end
     # Create a child organization
-    # Create a child organization.  This endpoint requires the [multi-organization account](https://docs.datadoghq.com/account_management/multi_organization/) feature and must be enabled by [contacting support](https://docs.datadoghq.com/help/).  Once a new child organization is created, you can interact with it by using the `org.public_id`, `pi_key.key`, and `application_key.hash` provided in the response.
+    # Create a child organization.  This endpoint requires the [multi-organization account](https://docs.datadoghq.com/account_management/multi_organization/) feature and must be enabled by [contacting support](https://docs.datadoghq.com/help/).  Once a new child organization is created, you can interact with it by using the `org.public_id`, `api_key.key`, and `application_key.hash` provided in the response.
     # @param body [OrganizationCreateBody] Organization object that needs to be created
     # @param [Hash] opts the optional parameters
     # @return [OrganizationCreateResponse]
@@ -33,7 +33,7 @@ module DatadogAPIClient::V1
     end
 
     # Create a child organization
-    # Create a child organization.  This endpoint requires the [multi-organization account](https://docs.datadoghq.com/account_management/multi_organization/) feature and must be enabled by [contacting support](https://docs.datadoghq.com/help/).  Once a new child organization is created, you can interact with it by using the &#x60;org.public_id&#x60;, &#x60;pi_key.key&#x60;, and &#x60;application_key.hash&#x60; provided in the response.
+    # Create a child organization.  This endpoint requires the [multi-organization account](https://docs.datadoghq.com/account_management/multi_organization/) feature and must be enabled by [contacting support](https://docs.datadoghq.com/help/).  Once a new child organization is created, you can interact with it by using the &#x60;org.public_id&#x60;, &#x60;api_key.key&#x60;, and &#x60;application_key.hash&#x60; provided in the response.
     # @param body [OrganizationCreateBody] Organization object that needs to be created
     # @param [Hash] opts the optional parameters
     # @return [Array<(OrganizationCreateResponse, Integer, Hash)>] OrganizationCreateResponse data, response status code and response headers
