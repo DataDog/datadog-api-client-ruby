0129a24ae803387d141aacadf61b511fdc3cac50
ci.datadog-api-spec <packages@datadoghq.com>
2021-09-28 14:28:30 +0000

    Regenerate client from commit 1f84aba of spec repo

--- lib/datadog_api_client/v1/models/downtime_recurrence.rb
+++ lib/datadog_api_client/v1/models/downtime_recurrence.rb
@@ -25,7 +25,7 @@ module DatadogAPIClient::V1
     # How often to repeat as an integer. For example, to repeat every 3 days, select a type of `days` and a period of `3`.
     attr_accessor :period
 
-    # The `RRULE` standard for defining recurring events (**requires to set \"type\" to rrule**) For example, to have a recurring event on the first day of each month, set the type to `rrule` and set the `FREQ` to `MONTHLY` and `BYMONTHDAY` to `1`. Most common `rrule` options from the [iCalendar Spec](https://tools.ietf.org/html/rfc5545) are supported.  **Note**: Attributes specifying the duration in `RRULE` are not supported (for example, `DTSTART`, `DTEND`, `DURATION`). More examples available in this [downtime guide](https://docs.datadoghq.com/monitors/guide/supress-alert-with-downtimes/?tab=api)
+    # The `RRULE` standard for defining recurring events (**requires to set \"type\" to rrule**) For example, to have a recurring event on the first day of each month, set the type to `rrule` and set the `FREQ` to `MONTHLY` and `BYMONTHDAY` to `1`. Most common `rrule` options from the [iCalendar Spec](https://tools.ietf.org/html/rfc5545) are supported.  **Note**: Attributes specifying the duration in `RRULE` are not supported (for example, `DTSTART`, `DTEND`, `DURATION`). More examples available in this [downtime guide](https://docs.datadoghq.com/monitors/guide/suppress-alert-with-downtimes/?tab=api)
     attr_accessor :rrule
 
     # The type of recurrence. Choose from `days`, `weeks`, `months`, `years`, `rrule`.
