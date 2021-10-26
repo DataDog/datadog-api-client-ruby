b6eb2d3a7edf376cfba761de53bc467113b01519
Jiri Kuncar <jiri.kuncar@gmail.com>
2021-10-26 15:04:21 +0200

    Migrate code generation

--- check-examples.sh
+++ check-examples.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]; then
-    echo "No arguments supplied"
-    exit 2
-fi
-
-./extract-code-blocks.sh examples $1
+./extract-code-blocks.sh examples
 
 bundle install
-ls examples/$1/*/*.rbbeta | xargs -P $(($(nproc)*2)) -n 1 bundle exec ruby -cw && bundle exec rubocop examples/$1/*/*.rbbeta
+ls examples/*/*/*.rbbeta | xargs -P $(($(nproc)*2)) -n 1 bundle exec ruby -cw && bundle exec rubocop examples/*/*/*.rbbeta
 if [ $? -ne 0 ]; then
     echo -e "Failed to build examples"
     exit 1
