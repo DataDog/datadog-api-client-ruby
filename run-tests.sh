0f22c01439a85cca4b2311c4da7ef6c4996c6640
Thomas Hervé <thomas.herve@datadoghq.com>
2021-11-04 13:12:23 +0100

    Don't run rspec tests

--- run-tests.sh
+++ run-tests.sh
@@ -5,12 +5,6 @@ function finish {
 trap finish EXIT
 
 bundle install
-bundle exec rspec
-RSPEC_RESULT=$?
-if [ "$RERECORD_FAILED_TESTS" == "true" ] && [ "$RSPEC_RESULT" -ne "0" ]; then
-    RECORD=true bundle exec rspec --only-failures
-    RSPEC_RESULT=$?
-fi
 bundle exec cucumber -t 'not @skip' -f rerun -o rerun.txt -f pretty
 CUCUMBER_RESULT=$?
 if [ "$RERECORD_FAILED_TESTS" == "true" ] && [ "$CUCUMBER_RESULT" -ne "0" ]; then
@@ -18,4 +12,4 @@ if [ "$RERECORD_FAILED_TESTS" == "true" ] && [ "$CUCUMBER_RESULT" -ne "0" ]; the
     CUCUMBER_RESULT=$?
 fi
 
-exit "$(($RSPEC_RESULT+$CUCUMBER_RESULT))"
+exit $CUCUMBER_RESULT
