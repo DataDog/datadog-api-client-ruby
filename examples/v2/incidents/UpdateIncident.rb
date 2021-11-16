b4598da0d49e5f9bef44c37690ad69facd21086e
ci.datadog-api-spec <packages@datadoghq.com>
2021-11-16 15:39:42 +0000

    Regenerate client from commit d85e5fe of spec repo

--- examples/v2/incidents/UpdateIncident.rb
+++ examples/v2/incidents/UpdateIncident.rb
@@ -10,15 +10,16 @@ INCIDENT_DATA_ID = ENV["INCIDENT_DATA_ID"]
 body = DatadogAPIClient::V2::IncidentUpdateRequest.new({
   data: DatadogAPIClient::V2::IncidentUpdateData.new({
     id: INCIDENT_DATA_ID,
-    type: DatadogAPIClient::V2::IncidentType::INCIDENTS,
-    attributes: DatadogAPIClient::V2::IncidentUpdateAttributes.new({
-      fields: DatadogAPIClient::V2::IncidentUpdateAttributesFields.new({
-        state: DatadogAPIClient::V2::IncidentFieldAttributesSingleValue.new({
-          type: DatadogAPIClient::V2::IncidentFieldAttributesSingleValueType::DROPDOWN,
-          value: 'resolved'
-        }) }),
-      title: 'A test incident title-updated'
-    })
+  type: DatadogAPIClient::V2::IncidentType::INCIDENTS,
+  attributes: DatadogAPIClient::V2::IncidentUpdateAttributes.new({
+    fields: DatadogAPIClient::V2::IncidentUpdateAttributesFields.new({
+      state: DatadogAPIClient::V2::IncidentFieldAttributesSingleValue.new({
+        type: DatadogAPIClient::V2::IncidentFieldAttributesSingleValueType::DROPDOWN,
+      value: 'resolved'
+      }) }),
+  title: 'A test incident title-updated'
+  })
   })
 })
 p api_instance.update_incident(INCIDENT_DATA_ID, body)
+CIDENT_DATA_ID, body)
