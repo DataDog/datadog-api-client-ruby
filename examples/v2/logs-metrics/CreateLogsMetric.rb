38e60bdc62abe7c5483657578a9a20d705de192c
ci.datadog-api-spec <packages@datadoghq.com>
2021-11-16 13:58:00 +0000

    Regenerate client from commit 4a09063 of spec repo

--- examples/v2/logs-metrics/CreateLogsMetric.rb
+++ examples/v2/logs-metrics/CreateLogsMetric.rb
@@ -6,12 +6,13 @@ api_instance = DatadogAPIClient::V2::LogsMetricsAPI.new
 body = DatadogAPIClient::V2::LogsMetricCreateRequest.new({
   data: DatadogAPIClient::V2::LogsMetricCreateData.new({
     id: 'Example-Create_a_log_based_metric_returns_OK_response',
-    type: DatadogAPIClient::V2::LogsMetricType::LOGS_METRICS,
-    attributes: DatadogAPIClient::V2::LogsMetricCreateAttributes.new({
-      compute: DatadogAPIClient::V2::LogsMetricCompute.new({
-        aggregation_type: DatadogAPIClient::V2::LogsMetricComputeAggregationType::COUNT
-      })
+  type: DatadogAPIClient::V2::LogsMetricType::LOGS_METRICS,
+  attributes: DatadogAPIClient::V2::LogsMetricCreateAttributes.new({
+    compute: DatadogAPIClient::V2::LogsMetricCompute.new({
+      aggregation_type: DatadogAPIClient::V2::LogsMetricComputeAggregationType::COUNT
     })
+  })
   })
 })
 p api_instance.create_logs_metric(body)
+dy)
