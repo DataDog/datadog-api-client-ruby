4280a6f0a0e824c43f783168c84ce39f5525320b
ci.datadog-api-spec <packages@datadoghq.com>
2021-11-17 09:22:03 +0000

    Regenerate client from commit 97af4ac of spec repo

--- examples/v2/users/UpdateUser.rb
+++ examples/v2/users/UpdateUser.rb
@@ -9,8 +9,12 @@ USER_DATA_ID = ENV["USER_DATA_ID"]
 body = DatadogAPIClient::V2::UserUpdateRequest.new({
   data: DatadogAPIClient::V2::UserUpdateData.new({
     id: USER_DATA_ID,
-    type: DatadogAPIClient::V2::UsersType::USERS,
-    attributes: DatadogAPIClient::V2::UserUpdateAttributes.new({ name: 'updated', disabled: true })
+  type: DatadogAPIClient::V2::UsersType::USERS,
+  attributes: DatadogAPIClient::V2::UserUpdateAttributes.new({
+    name: 'updated',
+  disabled: true
+  })
   })
 })
 p api_instance.update_user(USER_DATA_ID, body)
+body)
