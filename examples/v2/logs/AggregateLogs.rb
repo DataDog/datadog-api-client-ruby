ef249373f3b71f7b7fbb955ecfa91a50ba6e718e
ci.datadog-api-spec <packages@datadoghq.com>
2021-11-23 11:29:08 +0000

    Regenerate client from commit ea5d03a of spec repo

--- examples/v2/logs/AggregateLogs.rb
+++ examples/v2/logs/AggregateLogs.rb
@@ -4,6 +4,14 @@ require 'datadog_api_client'
 api_instance = DatadogAPIClient::V2::LogsAPI.new
 
 body = DatadogAPIClient::V2::LogsAggregateRequest.new({
-  filter: DatadogAPIClient::V2::LogsQueryFilter.new({ from: 'now-15m', indexes: ['main'], query: '*', to: 'now' })
+  filter: DatadogAPIClient::V2::LogsQueryFilter.new({
+    from: 'now-15m',
+  indexes: [
+    'main'
+  ],
+  query: '*',
+  to: 'now'
+  })
 })
 p api_instance.aggregate_logs(body)
+body)
