173bdf5667a251e8c7ac0f2b4b84805398c547f5
ci.datadog-api-spec <packages@datadoghq.com>
2022-01-05 18:07:50 +0000

    Regenerate client from commit dc856e9 of spec repo

--- examples/v2/logs/ListLogsGet_2034110533.rb
+++ examples/v2/logs/ListLogsGet_2034110533.rb
@@ -5,8 +5,8 @@ api_instance = DatadogAPIClient::V2::LogsAPI.new
 opts = {
   filter_query: "datadog-agent",
   filter_index: "main",
-  filter_from: "2020-09-17T11:48:36+01:00",
-  filter_to: "2020-09-17T12:48:36+01:00",
+  filter_from: "2020-09-17T10:48:36Z",
+  filter_to: "2020-09-17T11:48:36Z",
   page_limit: 5,
 }
 p api_instance.list_logs_get(opts)
