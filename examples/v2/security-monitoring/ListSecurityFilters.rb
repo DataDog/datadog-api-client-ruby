34d284d904f35ead4814d00de880fbf32a7b01a3
Jiri Kuncar <jiri.kuncar@gmail.com>
2022-01-17 15:49:17 +0100

    rm

--- examples/v2/security-monitoring/ListSecurityFilters.rb
+++
@@ -1,5 +0,0 @@
-# Get all security filters returns "OK" response
-
-require "datadog_api_client"
-api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
-p api_instance.list_security_filters()
