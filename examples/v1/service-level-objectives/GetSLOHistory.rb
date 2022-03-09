5cc2598c36bc57f4e1083b1c967020a1d3a1b50c
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-09 13:12:12 +0000

    Regenerate client from commit f9b0de6f of spec repo

---
+++ examples/v1/service-level-objectives/GetSLOHistory.rb
@@ -0,0 +1,11 @@
+# Get an SLO's history returns "OK" response
+
+require "datadog_api_client"
+DatadogAPIClient::V2.configure do |config|
+  config.unstable_operations[:get_slo_history] = true
+end
+api_instance = DatadogAPIClient::V1::ServiceLevelObjectivesAPI.new
+
+# there is a valid "slo" in the system
+SLO_DATA_0_ID = ENV["SLO_DATA_0_ID"]
+p api_instance.get_slo_history(SLO_DATA_0_ID, (Time.now + -1 * 86400).to_i, Time.now.to_i)
