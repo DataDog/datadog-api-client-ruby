a71190d7d2a8845fcb707def37cfb859cb155542
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-16 14:35:31 +0000

    Regenerate client from commit e10259da of spec repo

---
+++ examples/v1/logs/ListLogs_235998668.rb
@@ -0,0 +1,15 @@
+# Search test logs returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V1::LogsAPI.new
+
+body = DatadogAPIClient::V1::LogsListRequest.new({
+  index: "main",
+  query: "service:web*",
+  sort: DatadogAPIClient::V1::LogsSort::TIME_ASCENDING,
+  time: DatadogAPIClient::V1::LogsListRequestTime.new({
+    from: (Time.now + -1 * 3600),
+    to: Time.now,
+  }),
+})
+p api_instance.list_logs(body)
