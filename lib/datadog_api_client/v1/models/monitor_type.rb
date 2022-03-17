cb33eda2a6f51fe2dac820eca6218d7333a1a2d8
ci.datadog-api-spec <packages@datadoghq.com>
2022-03-17 20:07:58 +0000

    Regenerate client from commit 717103c5 of spec repo

--- lib/datadog_api_client/v1/models/monitor_type.rb
+++ lib/datadog_api_client/v1/models/monitor_type.rb
@@ -35,5 +35,6 @@ module DatadogAPIClient::V1
     EVENT_V2_ALERT = "event-v2 alert".freeze
     AUDIT_ALERT = "audit alert".freeze
     CI_PIPELINES_ALERT = "ci-pipelines alert".freeze
+    ERROR_TRACKING_ALERT = "error-tracking alert".freeze
   end
 end
