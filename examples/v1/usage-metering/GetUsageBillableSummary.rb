c39102985222fca0319c5823b3dc05040978c5f7
ci.datadog-api-spec <packages@datadoghq.com>
2022-04-05 11:15:08 +0000

    Regenerate client from commit 1bea0809 of spec repo

--- examples/v1/usage-metering/GetUsageBillableSummary.rb
+++ examples/v1/usage-metering/GetUsageBillableSummary.rb
@@ -2,7 +2,4 @@
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V1::UsageMeteringAPI.new
-opts = {
-  month: (Time.now + -3 * 86400),
-}
-p api_instance.get_usage_billable_summary(opts)
+p api_instance.get_usage_billable_summary()
