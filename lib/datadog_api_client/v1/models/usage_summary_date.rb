0e96080feba1e0e564de2f42901f68b7a9517329
ci.datadog-api-spec <packages@datadoghq.com>
2022-04-20 18:59:12 +0000

    Regenerate client from commit 8df9ca3b of spec repo

--- lib/datadog_api_client/v1/models/usage_summary_date.rb
+++ lib/datadog_api_client/v1/models/usage_summary_date.rb
@@ -166,6 +166,9 @@ module DatadogAPIClient::V1
     # Shows the 99th percentile of all distinct Networks hosts over all hours in the current date for all organizations.
     attr_accessor :npm_host_top99p
 
+    # Sum of all observability pipelines bytes processed over all hours in the current date for the given org.
+    attr_accessor :observability_pipelines_bytes_processed_sum
+
     # Sum of all online archived events over all hours in the current date for all organizations.
     attr_accessor :online_archive_events_count_sum
 
@@ -262,6 +265,7 @@ module DatadogAPIClient::V1
         :'mobile_rum_units_sum' => :'mobile_rum_units_sum',
         :'netflow_indexed_events_count_sum' => :'netflow_indexed_events_count_sum',
         :'npm_host_top99p' => :'npm_host_top99p',
+        :'observability_pipelines_bytes_processed_sum' => :'observability_pipelines_bytes_processed_sum',
         :'online_archive_events_count_sum' => :'online_archive_events_count_sum',
         :'opentelemetry_host_top99p' => :'opentelemetry_host_top99p',
         :'orgs' => :'orgs',
@@ -337,6 +341,7 @@ module DatadogAPIClient::V1
         :'mobile_rum_units_sum' => :'Integer',
         :'netflow_indexed_events_count_sum' => :'Integer',
         :'npm_host_top99p' => :'Integer',
+        :'observability_pipelines_bytes_processed_sum' => :'Integer',
         :'online_archive_events_count_sum' => :'Integer',
         :'opentelemetry_host_top99p' => :'Integer',
         :'orgs' => :'Array<UsageSummaryDateOrg>',
@@ -566,6 +571,10 @@ module DatadogAPIClient::V1
         self.npm_host_top99p = attributes[:'npm_host_top99p']
       end
 
+      if attributes.key?(:'observability_pipelines_bytes_processed_sum')
+        self.observability_pipelines_bytes_processed_sum = attributes[:'observability_pipelines_bytes_processed_sum']
+      end
+
       if attributes.key?(:'online_archive_events_count_sum')
         self.online_archive_events_count_sum = attributes[:'online_archive_events_count_sum']
       end
@@ -697,6 +706,7 @@ module DatadogAPIClient::V1
           mobile_rum_units_sum == o.mobile_rum_units_sum &&
           netflow_indexed_events_count_sum == o.netflow_indexed_events_count_sum &&
           npm_host_top99p == o.npm_host_top99p &&
+          observability_pipelines_bytes_processed_sum == o.observability_pipelines_bytes_processed_sum &&
           online_archive_events_count_sum == o.online_archive_events_count_sum &&
           opentelemetry_host_top99p == o.opentelemetry_host_top99p &&
           orgs == o.orgs &&
@@ -725,7 +735,7 @@ module DatadogAPIClient::V1
     # @return [Integer] Hash code
     # @!visibility private
     def hash
-      [agent_host_top99p, apm_azure_app_service_host_top99p, apm_host_top99p, audit_logs_lines_indexed_sum, avg_profiled_fargate_tasks, aws_host_top99p, aws_lambda_func_count, aws_lambda_invocations_sum, azure_app_service_top99p, billable_ingested_bytes_sum, browser_rum_lite_session_count_sum, browser_rum_replay_session_count_sum, browser_rum_units_sum, ci_pipeline_indexed_spans_sum, ci_test_indexed_spans_sum, ci_visibility_pipeline_committers_hwm, ci_visibility_test_committers_hwm, container_avg, container_hwm, cspm_aas_host_top99p, cspm_azure_host_top99p, cspm_container_avg, cspm_container_hwm, cspm_host_top99p, custom_ts_avg, cws_container_count_avg, cws_host_top99p, date, dbm_host_top99p, dbm_queries_count_avg, fargate_tasks_count_avg, fargate_tasks_count_hwm, gcp_host_top99p, heroku_host_top99p, incident_management_monthly_active_users_hwm, indexed_events_count_sum, infra_host_top99p, ingested_events_bytes_sum, iot_device_sum, iot_device_top99p, mobile_rum_lite_session_count_sum, mobile_rum_session_count_android_sum, mobile_rum_session_count_ios_sum, mobile_rum_session_count_sum, mobile_rum_units_sum, netflow_indexed_events_count_sum, npm_host_top99p, online_archive_events_count_sum, opentelemetry_host_top99p, orgs, profiling_host_top99p, rum_browser_and_mobile_session_count, rum_session_count_sum, rum_total_session_count_sum, rum_units_sum, sds_logs_scanned_bytes_sum, sds_total_scanned_bytes_sum, synthetics_browser_check_calls_count_sum, synthetics_check_calls_count_sum, trace_search_indexed_events_count_sum, twol_ingested_events_bytes_sum, vsphere_host_top99p].hash
+      [agent_host_top99p, apm_azure_app_service_host_top99p, apm_host_top99p, audit_logs_lines_indexed_sum, avg_profiled_fargate_tasks, aws_host_top99p, aws_lambda_func_count, aws_lambda_invocations_sum, azure_app_service_top99p, billable_ingested_bytes_sum, browser_rum_lite_session_count_sum, browser_rum_replay_session_count_sum, browser_rum_units_sum, ci_pipeline_indexed_spans_sum, ci_test_indexed_spans_sum, ci_visibility_pipeline_committers_hwm, ci_visibility_test_committers_hwm, container_avg, container_hwm, cspm_aas_host_top99p, cspm_azure_host_top99p, cspm_container_avg, cspm_container_hwm, cspm_host_top99p, custom_ts_avg, cws_container_count_avg, cws_host_top99p, date, dbm_host_top99p, dbm_queries_count_avg, fargate_tasks_count_avg, fargate_tasks_count_hwm, gcp_host_top99p, heroku_host_top99p, incident_management_monthly_active_users_hwm, indexed_events_count_sum, infra_host_top99p, ingested_events_bytes_sum, iot_device_sum, iot_device_top99p, mobile_rum_lite_session_count_sum, mobile_rum_session_count_android_sum, mobile_rum_session_count_ios_sum, mobile_rum_session_count_sum, mobile_rum_units_sum, netflow_indexed_events_count_sum, npm_host_top99p, observability_pipelines_bytes_processed_sum, online_archive_events_count_sum, opentelemetry_host_top99p, orgs, profiling_host_top99p, rum_browser_and_mobile_session_count, rum_session_count_sum, rum_total_session_count_sum, rum_units_sum, sds_logs_scanned_bytes_sum, sds_total_scanned_bytes_sum, synthetics_browser_check_calls_count_sum, synthetics_check_calls_count_sum, trace_search_indexed_events_count_sum, twol_ingested_events_bytes_sum, vsphere_host_top99p].hash
     end
   end
 end
