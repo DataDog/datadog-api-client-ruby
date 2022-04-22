f3e6d3e3ae0fc3997b217f840aa98044afc13f73
ci.datadog-api-spec <packages@datadoghq.com>
2022-04-22 09:15:05 +0000

    Regenerate client from commit 0d1b8737 of spec repo

--- examples/v1/organizations/UploadIdPForOrg.rb
+++ examples/v1/organizations/UploadIdPForOrg.rb
@@ -2,4 +2,4 @@
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V1::OrganizationsAPI.new
-p api_instance.upload_idp_for_org("abc123", File.open("./idp_metadata.xml", "r"))
+p api_instance.upload_id_p_for_org("abc123", File.open("./idp_metadata.xml", "r"))
--- examples/v2/authn-mappings/GetAuthNMapping.rb
+++ examples/v2/authn-mappings/GetAuthNMapping.rb
@@ -5,4 +5,4 @@ api_instance = DatadogAPIClient::V2::AuthNMappingsAPI.new
 
 # there is a valid "authn_mapping" in the system
 AUTHN_MAPPING_DATA_ID = ENV["AUTHN_MAPPING_DATA_ID"]
-p api_instance.get_authn_mapping(AUTHN_MAPPING_DATA_ID)
+p api_instance.get_auth_n_mapping(AUTHN_MAPPING_DATA_ID)
