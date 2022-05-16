1e31482bd6a7f7cea8efbd033431d735835a4fb2
ci.datadog-api-spec <packages@datadoghq.com>
2022-05-16 16:35:32 +0000

    Regenerate client from commit e6216ca6 of spec repo

---
+++ examples/v2/metrics/EstimateMetricsOutputSeries.rb
@@ -0,0 +1,9 @@
+# Estimate Output Series - Public v2 API returns "Success" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::MetricsAPI.new
+opts = {
+  filter_groups: "app,host",
+  filter_num_aggregations: 4,
+}
+p api_instance.estimate_metrics_output_series("system.cpu.idle", opts)
