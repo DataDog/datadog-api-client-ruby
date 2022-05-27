f2a22f248cb479da379e6a2267ad56dcf8a2d139
Thomas Hervé <thomas.herve@datadoghq.com>
2022-05-27 14:09:01 +0200

    Enable header matching in tests
    Enable header matching against cassettes, and fix multiple headers
    management in the client.

--- lib/datadog_api_client/v1/api_client.rb
+++ lib/datadog_api_client/v1/api_client.rb
@@ -36,7 +36,6 @@ module DatadogAPIClient::V1
       @config = config
       @user_agent = "#{DatadogAPIClient::USER_AGENT}"
       @default_headers = {
-        'Content-Type' => 'application/json',
         'User-Agent' => @user_agent
       }
       @default_headers['Accept-Encoding'] = 'gzip' if @config.compress
@@ -340,10 +339,7 @@ module DatadogAPIClient::V1
     # @param [Array] accepts array for Accept
     # @return [String] the Accept header (e.g. application/json)
     def select_header_accept(accepts)
-      return nil if accepts.nil? || accepts.empty?
-      # use JSON when present, otherwise use all of the provided
-      json_accept = accepts.find { |s| json_mime?(s) }
-      json_accept || accepts.join(',')
+      accepts.join(', ')
     end
 
     # Return Content-Type header based on an array of content types provided.
