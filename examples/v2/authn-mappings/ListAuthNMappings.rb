512f42e4729256c9e6533c6a0ca597807e279a74
ci.datadog-api-spec <packages@datadoghq.com>
2022-06-03 15:37:53 +0000

    Regenerate client from commit b6edc832 of spec repo

--- examples/v2/authn-mappings/ListAuthNMappings.rb
+++ examples/v2/authn-mappings/ListAuthNMappings.rb
@@ -2,4 +2,10 @@
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V2::AuthNMappingsAPI.new
-p api_instance.list_authn_mappings()
+
+# there is a valid "authn_mapping" in the system
+AUTHN_MAPPING_DATA_ATTRIBUTES_ATTRIBUTE_KEY = ENV["AUTHN_MAPPING_DATA_ATTRIBUTES_ATTRIBUTE_KEY"]
+opts = {
+  filter: AUTHN_MAPPING_DATA_ATTRIBUTES_ATTRIBUTE_KEY,
+}
+p api_instance.list_authn_mappings(opts)
--- examples/v2/key-management/GetAPIKey.rb
+++ examples/v2/key-management/GetAPIKey.rb
@@ -5,4 +5,7 @@ api_instance = DatadogAPIClient::V2::KeyManagementAPI.new
 
 # there is a valid "api_key" in the system
 API_KEY_DATA_ID = ENV["API_KEY_DATA_ID"]
-p api_instance.get_api_key(API_KEY_DATA_ID)
+opts = {
+  include: "created_by,modified_by",
+}
+p api_instance.get_api_key(API_KEY_DATA_ID, opts)
