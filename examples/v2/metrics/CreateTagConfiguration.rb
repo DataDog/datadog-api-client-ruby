6d7b74e5d3cb8551f7f9f922116ab80b8fc6f564
ci.datadog-api-spec <packages@datadoghq.com>
2022-06-06 19:58:02 +0000

    Regenerate client from commit 47ec2394 of spec repo

--- examples/v2/metrics/CreateTagConfiguration.rb
+++ examples/v2/metrics/CreateTagConfiguration.rb
@@ -1,6 +1,9 @@
 # Create a tag configuration returns "Created" response
 
 require "datadog_api_client"
+DatadogAPIClient::V2.configure do |config|
+  config.unstable_operations[:create_tag_configuration] = true
+end
 api_instance = DatadogAPIClient::V2::MetricsAPI.new
 
 body = DatadogAPIClient::V2::MetricTagConfigurationCreateRequest.new({
--- examples/v2/metrics/DeleteTagConfiguration.rb
+++ examples/v2/metrics/DeleteTagConfiguration.rb
@@ -1,5 +1,8 @@
 # Delete a tag configuration returns "No Content" response
 
 require "datadog_api_client"
+DatadogAPIClient::V2.configure do |config|
+  config.unstable_operations[:delete_tag_configuration] = true
+end
 api_instance = DatadogAPIClient::V2::MetricsAPI.new
 api_instance.delete_tag_configuration("ExampleDeleteatagconfigurationreturnsNoContentresponse")
