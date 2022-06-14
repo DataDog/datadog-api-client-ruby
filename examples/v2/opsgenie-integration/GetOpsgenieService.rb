bf7e3435e528e1ff4fa628f6dcb29260ee764739
ci.datadog-api-spec <packages@datadoghq.com>
2022-06-14 08:16:45 +0000

    Regenerate client from commit 0f6264a5 of spec repo

--- examples/v2/opsgenie-integration/GetOpsgenieService.rb
+++ examples/v2/opsgenie-integration/GetOpsgenieService.rb
@@ -2,4 +2,7 @@
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V2::OpsgenieIntegrationAPI.new
-p api_instance.get_opsgenie_service("integration_service_id")
+
+# there is a valid "opsgenie_service" in the system
+OPSGENIE_SERVICE_DATA_ID = ENV["OPSGENIE_SERVICE_DATA_ID"]
+p api_instance.get_opsgenie_service(OPSGENIE_SERVICE_DATA_ID)
