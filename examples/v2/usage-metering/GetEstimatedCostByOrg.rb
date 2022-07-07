b22129bf63682f3ef2885385c1bb856a91b1b3b4
ci.datadog-api-spec <packages@datadoghq.com>
2022-07-07 20:17:09 +0000

    Regenerate client from commit 3b40e043 of spec repo

--- examples/v2/usage-metering/GetEstimatedCostByOrg.rb
+++
@@ -1,5 +0,0 @@
-# Get estimated cost across multi-org account returns "OK" response
-
-require "datadog_api_client"
-api_instance = DatadogAPIClient::V2::UsageMeteringAPI.new
-p api_instance.get_estimated_cost_by_org()
