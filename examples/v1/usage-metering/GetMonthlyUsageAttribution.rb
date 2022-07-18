4133cf97923d6d3c3cfd2c93ba50361080570f44
ci.datadog-api-spec <packages@datadoghq.com>
2022-07-18 17:54:44 +0000

    Regenerate client from commit f486a915 of spec repo

--- examples/v1/usage-metering/GetMonthlyUsageAttribution.rb
+++ examples/v1/usage-metering/GetMonthlyUsageAttribution.rb
@@ -1,8 +1,5 @@
 # Get monthly usage attribution returns "OK" response
 
 require "datadog_api_client"
-DatadogAPIClient::V2.configure do |config|
-  config.unstable_operations[:get_monthly_usage_attribution] = true
-end
 api_instance = DatadogAPIClient::V1::UsageMeteringAPI.new
 p api_instance.get_monthly_usage_attribution((Time.now + -3 * 86400), MonthlyUsageAttributionSupportedMetrics::INFRA_HOST_USAGE)
