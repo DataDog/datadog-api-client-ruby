ddf8068fa1e8f67e9572767590af947d183740d0
Sherzod K <sherzod.karimov@datadoghq.com>
2022-07-26 16:28:49 -0400

    generate pagination examples

---
+++ examples/v2/events/ListEvents_1527584014.rb
@@ -0,0 +1,13 @@
+# Get a list of events returns "OK" response with pagination
+
+require "datadog_api_client"
+DatadogAPIClient::V2.configure do |config|
+  config.unstable_operations[:list_events] = true
+end
+api_instance = DatadogAPIClient::V2::EventsAPI.new
+opts = {
+  filter_from: "now-15m",
+  filter_to: "now",
+  page_limit: 2,
+}
+api_instance.list_events_with_pagination(opts) { |item| puts item }
