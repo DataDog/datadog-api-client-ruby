59d0d698b4c560f65dd992854920d6fec6b791dd
Sherzod Karimov <sherzod.karimov@datadoghq.com>
2022-08-02 22:03:26 -0400

    don't try to load model_base with zeitwerk

--- lib/datadog_api_client.rb
+++ lib/datadog_api_client.rb
@@ -7,5 +7,7 @@ loader.collapse("#{__dir__}/datadog_api_client/*/models/")
 loader.collapse("#{__dir__}/datadog_api_client/*/api/")
 loader.push_dir("#{__dir__}/datadog_api_client/v1", namespace: DatadogAPIClient::V1)
 loader.push_dir("#{__dir__}/datadog_api_client/v2", namespace: DatadogAPIClient::V2)
+# Ignore model_base.rb from being loaded in by zeitwerk as it is loaded in by default
+loader.ignore("#{__dir__}/datadog_api_client/*/model_base.rb")
 loader.inflector = DatadogAPIClient::DatadogAPIClientInflector.new
 loader.setup
