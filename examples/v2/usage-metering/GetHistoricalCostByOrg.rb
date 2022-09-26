5800a0eee185335a76936640b49dcec8c9ef644c
ci.datadog-api-spec <packages@datadoghq.com>
2022-09-26 15:36:19 +0000

    Regenerate client from commit c7261a1b of spec repo

---
+++ examples/v2/usage-metering/GetHistoricalCostByOrg.rb
@@ -0,0 +1,8 @@
+# Get historical cost across your account returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::UsageMeteringAPI.new
+opts = {
+  view: "sub-org",
+}
+p api_instance.get_historical_cost_by_org((Time.now + -1 * 60), opts)
