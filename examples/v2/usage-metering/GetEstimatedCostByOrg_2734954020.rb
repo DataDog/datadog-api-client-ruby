9749862344cf9cbccdc9a1c6582e3ec90d570896
ci.datadog-api-spec <packages@datadoghq.com>
2022-11-04 18:42:43 +0000

    Regenerate client from commit af3f11fc of spec repo

--- examples/v2/usage-metering/GetEstimatedCostByOrg_2734954020.rb
+++
@@ -1,9 +0,0 @@
-# GetEstimatedCostByOrg with start_date returns "OK" response
-
-require "datadog_api_client"
-api_instance = DatadogAPIClient::V2::UsageMeteringAPI.new
-opts = {
-  view: "sub-org",
-  start_date: (Time.now + -5 * 86400),
-}
-p api_instance.get_estimated_cost_by_org(opts)
