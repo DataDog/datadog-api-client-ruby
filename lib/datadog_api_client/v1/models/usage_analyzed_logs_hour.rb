b55e406f78b1ae7b4422fcc770905a34efd408ff
ci.datadog-api-spec <packages@datadoghq.com>
2023-05-05 15:03:12 +0000

    Regenerate client from commit 380840ff of spec repo

--- lib/datadog_api_client/v1/models/usage_analyzed_logs_hour.rb
+++ lib/datadog_api_client/v1/models/usage_analyzed_logs_hour.rb
@@ -59,6 +59,14 @@ module DatadogAPIClient::V1
       }
     end
 
+    # List of attributes with nullable: true
+    # @!visibility private
+    def self.openapi_nullable
+      Set.new([
+        :'analyzed_logs',
+      ])
+    end
+
     # Initializes the object
     # @param attributes [Hash] Model attributes in the form of hash
     # @!visibility private
