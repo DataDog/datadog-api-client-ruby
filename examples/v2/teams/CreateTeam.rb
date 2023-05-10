1c05ae735b4e004f8d41d7515dd7f48a33fd7d6d
ci.datadog-api-spec <packages@datadoghq.com>
2023-05-10 16:32:10 +0000

    Regenerate client from commit b590b791 of spec repo

--- examples/v2/teams/CreateTeam.rb
+++ examples/v2/teams/CreateTeam.rb
@@ -6,8 +6,8 @@ api_instance = DatadogAPIClient::V2::TeamsAPI.new
 body = DatadogAPIClient::V2::TeamCreateRequest.new({
   data: DatadogAPIClient::V2::TeamCreate.new({
     attributes: DatadogAPIClient::V2::TeamCreateAttributes.new({
-      handle: "1636629071",
-      name: "1636629071",
+      handle: "handle-a0fc0297eb519635",
+      name: "name-a0fc0297eb519635",
     }),
     relationships: DatadogAPIClient::V2::TeamCreateRelationships.new({
       users: DatadogAPIClient::V2::RelationshipToUsers.new({
