e29ca2d1513507cad3b6601804af9de4c7386293
ci.datadog-api-spec <packages@datadoghq.com>
2023-06-12 13:40:23 +0000

    Regenerate client from commit 2c47742f of spec repo

---
+++ examples/v1/synthetics/GetSyntheticsDefaultLocations.rb
@@ -0,0 +1,5 @@
+# Get the default locations returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V1::SyntheticsAPI.new
+p api_instance.get_synthetics_default_locations()
