a98778a35920d80608003f019e6b4e6b22b8971b
ci.datadog-api-spec <packages@datadoghq.com>
2023-07-20 19:51:25 +0000

    Regenerate client from commit 5cc4eb6d of spec repo

--- lib/datadog_api_client/v2/api/ip_allowlist_api.rb
+++ lib/datadog_api_client/v2/api/ip_allowlist_api.rb
@@ -63,7 +63,7 @@ module DatadogAPIClient::V2
       return_type = opts[:debug_return_type] || 'IPAllowlistResponse'
 
       # auth_names
-      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth, :AuthZ]
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth]
 
       new_options = opts.merge(
         :operation => :get_ip_allowlist,
@@ -130,7 +130,7 @@ module DatadogAPIClient::V2
       return_type = opts[:debug_return_type] || 'IPAllowlistResponse'
 
       # auth_names
-      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth, :AuthZ]
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth]
 
       new_options = opts.merge(
         :operation => :update_ip_allowlist,
