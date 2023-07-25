1ca902d5ff9dfcb0bb0c25e77114782c2013f97c
ci.datadog-api-spec <packages@datadoghq.com>
2023-07-25 21:18:27 +0000

    Regenerate client from commit 91969c8e of spec repo

--- lib/datadog_api_client/v2/models/query_formula.rb
+++ lib/datadog_api_client/v2/models/query_formula.rb
@@ -25,6 +25,7 @@ module DatadogAPIClient::V2
     attr_reader :formula
 
     # Message for specifying limits to the number of values returned by a query.
+    # This limit is only for scalar queries and has no effect on timeseries queries.
     attr_accessor :limit
 
     # Attribute mapping from ruby-style variable name to JSON key.
