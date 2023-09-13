b783d5e5aa01882437b122ba4a496df4654ebf4b
ci.datadog-api-spec <packages@datadoghq.com>
2023-09-13 14:37:54 +0000

    Regenerate client from commit ec46249d of spec repo

--- lib/datadog_api_client/v1/models/heat_map_widget_request.rb
+++ lib/datadog_api_client/v1/models/heat_map_widget_request.rb
@@ -27,6 +27,9 @@ module DatadogAPIClient::V1
     # The event query.
     attr_accessor :event_query
 
+    # List of formulas that operate on queries.
+    attr_accessor :formulas
+
     # The log query.
     attr_accessor :log_query
 
@@ -41,6 +44,12 @@ module DatadogAPIClient::V1
 
     # Widget query.
     attr_accessor :q
+
+    # List of queries that can be returned directly or used in formulas.
+    attr_accessor :queries
+
+    # Timeseries, scalar, or event list response. Event list response formats are supported by Geomap widgets.
+    attr_accessor :response_format
 
     # The log query.
     attr_accessor :rum_query
@@ -57,11 +66,14 @@ module DatadogAPIClient::V1
       {
         :'apm_query' => :'apm_query',
         :'event_query' => :'event_query',
+        :'formulas' => :'formulas',
         :'log_query' => :'log_query',
         :'network_query' => :'network_query',
         :'process_query' => :'process_query',
         :'profile_metrics_query' => :'profile_metrics_query',
         :'q' => :'q',
+        :'queries' => :'queries',
+        :'response_format' => :'response_format',
         :'rum_query' => :'rum_query',
         :'security_query' => :'security_query',
         :'style' => :'style'
@@ -74,11 +86,14 @@ module DatadogAPIClient::V1
       {
         :'apm_query' => :'LogQueryDefinition',
         :'event_query' => :'EventQueryDefinition',
+        :'formulas' => :'Array<WidgetFormula>',
         :'log_query' => :'LogQueryDefinition',
         :'network_query' => :'LogQueryDefinition',
         :'process_query' => :'ProcessQueryDefinition',
         :'profile_metrics_query' => :'LogQueryDefinition',
         :'q' => :'String',
+        :'queries' => :'Array<FormulaAndFunctionQueryDefinition>',
+        :'response_format' => :'FormulaAndFunctionResponseFormat',
         :'rum_query' => :'LogQueryDefinition',
         :'security_query' => :'LogQueryDefinition',
         :'style' => :'WidgetStyle'
@@ -109,6 +124,12 @@ module DatadogAPIClient::V1
         self.event_query = attributes[:'event_query']
       end
 
+      if attributes.key?(:'formulas')
+        if (value = attributes[:'formulas']).is_a?(Array)
+          self.formulas = value
+        end
+      end
+
       if attributes.key?(:'log_query')
         self.log_query = attributes[:'log_query']
       end
@@ -127,6 +148,16 @@ module DatadogAPIClient::V1
 
       if attributes.key?(:'q')
         self.q = attributes[:'q']
+      end
+
+      if attributes.key?(:'queries')
+        if (value = attributes[:'queries']).is_a?(Array)
+          self.queries = value
+        end
+      end
+
+      if attributes.key?(:'response_format')
+        self.response_format = attributes[:'response_format']
       end
 
       if attributes.key?(:'rum_query')
@@ -150,11 +181,14 @@ module DatadogAPIClient::V1
       self.class == o.class &&
           apm_query == o.apm_query &&
           event_query == o.event_query &&
+          formulas == o.formulas &&
           log_query == o.log_query &&
           network_query == o.network_query &&
           process_query == o.process_query &&
           profile_metrics_query == o.profile_metrics_query &&
           q == o.q &&
+          queries == o.queries &&
+          response_format == o.response_format &&
           rum_query == o.rum_query &&
           security_query == o.security_query &&
           style == o.style
@@ -164,7 +198,7 @@ module DatadogAPIClient::V1
     # @return [Integer] Hash code
     # @!visibility private
     def hash
-      [apm_query, event_query, log_query, network_query, process_query, profile_metrics_query, q, rum_query, security_query, style].hash
+      [apm_query, event_query, formulas, log_query, network_query, process_query, profile_metrics_query, q, queries, response_format, rum_query, security_query, style].hash
     end
   end
 end
