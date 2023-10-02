f822035712a80dee6575774a9c2c963b4acc4dde
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-02 15:36:01 +0000

    Regenerate client from commit deb69b98 of spec repo

---
+++ examples/v2/gcp-integration/CreateGCPSTSAccount_109518525.rb
@@ -0,0 +1,19 @@
+# Create a new entry for your service account with account_tags returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::GCPIntegrationAPI.new
+
+body = DatadogAPIClient::V2::GCPSTSServiceAccountCreateRequest.new({
+  data: DatadogAPIClient::V2::GCPSTSServiceAccountData.new({
+    attributes: DatadogAPIClient::V2::GCPSTSServiceAccountAttributes.new({
+      account_tags: [
+        "lorem",
+        "ipsum",
+      ],
+      client_email: "252bf553ef04b351@test-project.iam.gserviceaccount.com",
+      host_filters: [],
+    }),
+    type: DatadogAPIClient::V2::GCPServiceAccountType::GCP_SERVICE_ACCOUNT,
+  }),
+})
+p api_instance.create_gcpsts_account(body)
