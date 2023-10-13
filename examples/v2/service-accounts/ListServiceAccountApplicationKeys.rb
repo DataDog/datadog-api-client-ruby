66bb3477adee7d6d6aec519c3276ac3fbf2662c0
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-13 08:14:11 +0000

    Regenerate client from commit 30a99088 of spec repo

--- examples/v2/service-accounts/ListServiceAccountApplicationKeys.rb
+++ examples/v2/service-accounts/ListServiceAccountApplicationKeys.rb
@@ -2,4 +2,7 @@
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V2::ServiceAccountsAPI.new
-p api_instance.list_service_account_application_keys("00000000-0000-1234-0000-000000000000")
+
+# there is a valid "service_account_user" in the system
+SERVICE_ACCOUNT_USER_DATA_ID = ENV["SERVICE_ACCOUNT_USER_DATA_ID"]
+p api_instance.list_service_account_application_keys(SERVICE_ACCOUNT_USER_DATA_ID)
