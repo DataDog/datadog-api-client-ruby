c1e910da04d59338693f707e2f373dbe13e006aa
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-18 20:17:23 +0000

    Regenerate client from commit a1e5664c of spec repo

---
+++ examples/v2/containers/ListContainers_931009654.rb
@@ -0,0 +1,11 @@
+# Get All Containers returns "OK" response with pagination
+
+require "datadog_api_client"
+DatadogAPIClient.configure do |config|
+  config.unstable_operations["v2.list_containers".to_sym] = true
+end
+api_instance = DatadogAPIClient::V2::ContainersAPI.new
+opts = {
+  page_size: 2,
+}
+api_instance.list_containers_with_pagination(opts) { |item| puts item }
