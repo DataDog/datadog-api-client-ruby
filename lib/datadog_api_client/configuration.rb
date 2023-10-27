945fb1dee5f676292c4849827843558e72e4f0c7
ci.datadog-api-spec <packages@datadoghq.com>
2023-10-27 08:10:35 +0000

    Regenerate client from commit 86d56f2b of spec repo

--- lib/datadog_api_client/configuration.rb
+++ lib/datadog_api_client/configuration.rb
@@ -189,6 +189,7 @@ module DatadogAPIClient
       @http_proxypass = nil
       @logger = defined?(Rails) ? Rails.logger : Logger.new(STDOUT)
       @unstable_operations = {
+            "v2.create_ci_app_pipeline_event": false,
             "v2.list_container_images": false,
             "v2.list_containers": false,
             "v2.list_events": false,
