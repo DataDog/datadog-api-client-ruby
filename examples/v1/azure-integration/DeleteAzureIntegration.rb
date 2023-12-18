5f8e00ed361613e38e8b25949acc6193ba31cece
ci.datadog-api-spec <packages@datadoghq.com>
2023-12-18 13:47:26 +0000

    Regenerate client from commit f363f7b4 of spec repo

--- examples/v1/azure-integration/DeleteAzureIntegration.rb
+++ examples/v1/azure-integration/DeleteAzureIntegration.rb
@@ -4,20 +4,7 @@ require "datadog_api_client"
 api_instance = DatadogAPIClient::V1::AzureIntegrationAPI.new
 
 body = DatadogAPIClient::V1::AzureAccount.new({
-  app_service_plan_filters: "key:value,filter:example",
-  automute: true,
   client_id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-  client_secret: "testingx./Sw*g/Y33t..R1cH+hScMDt",
-  container_app_filters: "key:value,filter:example",
-  cspm_enabled: true,
-  custom_metrics_enabled: true,
-  errors: [
-    "*",
-  ],
-  host_filters: "key:value,filter:example",
-  new_client_id: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-  new_tenant_name: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
-  resource_collection_enabled: true,
   tenant_name: "9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d",
 })
 p api_instance.delete_azure_integration(body)
