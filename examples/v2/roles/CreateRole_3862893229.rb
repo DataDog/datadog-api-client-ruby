5c2d56d6410ce039e626a5f9dc8a1c2412bb6731
ci.datadog-api-spec <packages@datadoghq.com>
2024-04-21 03:07:11 +0000

    Regenerate client from commit 71b7fbd9 of spec repo

--- examples/v2/roles/CreateRole_3862893229.rb
+++ examples/v2/roles/CreateRole_3862893229.rb
@@ -1,26 +1,24 @@
-# Create role returns "OK" response
+# Create role with a permission returns "OK" response
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V2::RolesAPI.new
 
 body = DatadogAPIClient::V2::RoleCreateRequest.new({
   data: DatadogAPIClient::V2::RoleCreateData.new({
+    type: DatadogAPIClient::V2::RolesType::ROLES,
     attributes: DatadogAPIClient::V2::RoleCreateAttributes.new({
-      name: "developers",
+      name: "Example-Role",
     }),
     relationships: DatadogAPIClient::V2::RoleRelationships.new({
       permissions: DatadogAPIClient::V2::RelationshipToPermissions.new({
         data: [
           DatadogAPIClient::V2::RelationshipToPermissionData.new({
+            id: "d90f6831-d3d8-11e9-a77a-4fd230ddbc6a",
             type: DatadogAPIClient::V2::PermissionsType::PERMISSIONS,
           }),
         ],
       }),
-      users: DatadogAPIClient::V2::RelationshipToUsers.new({
-        data: [],
-      }),
     }),
-    type: DatadogAPIClient::V2::RolesType::ROLES,
   }),
 })
 p api_instance.create_role(body)
