c50315a4d51d9b24ec84045f8e086f120eccdab7
ci.datadog-api-spec <packages@datadoghq.com>
2024-06-18 16:07:54 +0000

    pre-commit fixes

--- examples/v2/api-management/ListAPIs.rb
+++ examples/v2/api-management/ListAPIs.rb
@@ -2,7 +2,7 @@
 
 require "datadog_api_client"
 DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.list_ap_is".to_sym] = true
+  config.unstable_operations["v2.list_apis".to_sym] = true
 end
 api_instance = DatadogAPIClient::V2::APIManagementAPI.new
-p api_instance.list_ap_is()
+p api_instance.list_apis()
