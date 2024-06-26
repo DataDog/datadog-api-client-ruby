3b7864fdb53c8a14361b04e61e4134053391ca65
ci.datadog-api-spec <packages@datadoghq.com>
2024-06-26 20:45:16 +0000

    Regenerate client from commit d588dddf of spec repo

---
+++ examples/v2/security-monitoring/ConvertExistingSecurityMonitoringRule.rb
@@ -0,0 +1,8 @@
+# Convert an existing rule from JSON to Terraform returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
+
+# there is a valid "security_rule" in the system
+SECURITY_RULE_ID = ENV["SECURITY_RULE_ID"]
+p api_instance.convert_existing_security_monitoring_rule(SECURITY_RULE_ID)
