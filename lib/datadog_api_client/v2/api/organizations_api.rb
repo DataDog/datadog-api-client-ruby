735cdd2cfed03db137a1140c4c5894251b8ed09b
ci.datadog-api-spec <packages@datadoghq.com>
2024-07-16 18:49:58 +0000

    Regenerate client from commit a669b93e of spec repo

--- lib/datadog_api_client/v2/api/organizations_api.rb
+++ lib/datadog_api_client/v2/api/organizations_api.rb
@@ -200,7 +200,7 @@ module DatadogAPIClient::V2
       return_type = opts[:debug_return_type] || 'OrgConfigGetResponse'
 
       # auth_names
-      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth, :AuthZ]
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth]
 
       new_options = opts.merge(
         :operation => :update_org_config,
