a5410c8715a2f7c8787c77b2a4b67e4934c6e17d
ci.datadog-api-spec <packages@datadoghq.com>
2024-08-14 22:32:32 +0000

    Regenerate client from commit 07d72513 of spec repo

--- lib/datadog_api_client/v2/models/application_key_response_included_item.rb
+++ lib/datadog_api_client/v2/models/application_key_response_included_item.rb
@@ -27,7 +27,8 @@ module DatadogAPIClient::V2
       def openapi_one_of
         [
           :'User',
-          :'Role'
+          :'Role',
+          :'LeakedKey'
         ]
       end
       # Builds the object
