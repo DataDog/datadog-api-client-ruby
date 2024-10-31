a378b3f46f1b9c73beb9aaea610e7e85f5a92f70
ci.datadog-api-spec <packages@datadoghq.com>
2024-10-31 16:42:05 +0000

    Regenerate client from commit 63c441b2 of spec repo

--- examples/v2/cloud-cost-management/ListAWSRelatedAccounts.rb
+++
@@ -1,5 +0,0 @@
-# List related AWS accounts returns "OK" response
-
-require "datadog_api_client"
-api_instance = DatadogAPIClient::V2::CloudCostManagementAPI.new
-p api_instance.list_aws_related_accounts("123456789123")
