99a748c04fbfc3a1d5c5e0eae67ce4b110caefcc
ci.datadog-api-spec <packages@datadoghq.com>
2024-11-25 22:28:51 +0000

    Regenerate client from commit a5271b93 of spec repo

--- examples/v2/ci-visibility-pipelines/CreateCIAppPipelineEvent_2341150096.rb
+++ examples/v2/ci-visibility-pipelines/CreateCIAppPipelineEvent_2341150096.rb
@@ -1,4 +1,4 @@
-# Send pipeline event returns "Request accepted for processing" response
+# Send running pipeline event returns "Request accepted for processing" response
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V2::CIVisibilityPipelinesAPI.new
@@ -6,14 +6,13 @@ api_instance = DatadogAPIClient::V2::CIVisibilityPipelinesAPI.new
 body = DatadogAPIClient::V2::CIAppCreatePipelineEventRequest.new({
   data: DatadogAPIClient::V2::CIAppCreatePipelineEventRequestData.new({
     attributes: DatadogAPIClient::V2::CIAppCreatePipelineEventRequestAttributes.new({
-      resource: DatadogAPIClient::V2::CIAppPipelineEventFinishedPipeline.new({
+      resource: DatadogAPIClient::V2::CIAppPipelineEventInProgressPipeline.new({
         level: DatadogAPIClient::V2::CIAppPipelineEventPipelineLevel::PIPELINE,
         unique_id: "3eacb6f3-ff04-4e10-8a9c-46e6d054024a",
         name: "Deploy to AWS",
         url: "https://my-ci-provider.example/pipelines/my-pipeline/run/1",
         start: (Time.now + -120),
-        _end: (Time.now + -30),
-        status: DatadogAPIClient::V2::CIAppPipelineEventPipelineStatus::SUCCESS,
+        status: DatadogAPIClient::V2::CIAppPipelineEventPipelineInProgressStatus::RUNNING,
         partial_retry: false,
         git: DatadogAPIClient::V2::CIAppGitInfo.new({
           repository_url: "https://github.com/DataDog/datadog-agent",
