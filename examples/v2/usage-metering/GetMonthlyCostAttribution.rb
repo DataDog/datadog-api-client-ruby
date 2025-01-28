178d904050a9679ccb37e92bd51b0179ccafa078
ci.datadog-api-spec <packages@datadoghq.com>
2025-01-28 19:56:26 +0000

    Regenerate client from commit a492a6f8 of spec repo

--- examples/v2/usage-metering/GetMonthlyCostAttribution.rb
+++ examples/v2/usage-metering/GetMonthlyCostAttribution.rb
@@ -1,9 +1,6 @@
 # Get Monthly Cost Attribution returns "OK" response
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.get_monthly_cost_attribution".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::UsageMeteringAPI.new
 opts = {
   end_month: (Time.now + -3 * 86400),
