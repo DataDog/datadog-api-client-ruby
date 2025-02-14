33c6369ac59bf78834e1ddb8577d95562720972b
ci.datadog-api-spec <packages@datadoghq.com>
2025-02-14 15:04:14 +0000

    Regenerate client from commit a739b49f of spec repo

--- examples/v2/security-monitoring/GetSBOM.rb
+++ examples/v2/security-monitoring/GetSBOM.rb
@@ -1,5 +1,8 @@
 # Get SBOM returns "OK" response
 
 require "datadog_api_client"
+DatadogAPIClient.configure do |config|
+  config.unstable_operations["v2.get_sbom".to_sym] = true
+end
 api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 p api_instance.get_sbom(AssetType::REPOSITORY, "github.com/datadog/datadog-agent")
