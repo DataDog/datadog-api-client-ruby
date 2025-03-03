011d3f5e91c45f05db9e003da0dc7d991922b3b8
ci.datadog-api-spec <packages@datadoghq.com>
2025-03-03 19:12:50 +0000

    Regenerate client from commit 35df584d of spec repo

--- examples/v2/app-builder/PublishApp.rb
+++ examples/v2/app-builder/PublishApp.rb
@@ -1,9 +1,6 @@
 # Publish App returns "Created" response
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.publish_app".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::AppBuilderAPI.new
 
 # there is a valid "app" in the system
--- examples/v2/app-builder/UnpublishApp.rb
+++ examples/v2/app-builder/UnpublishApp.rb
@@ -1,9 +1,6 @@
 # Unpublish App returns "OK" response
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.unpublish_app".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::AppBuilderAPI.new
 
 # there is a valid "app" in the system
