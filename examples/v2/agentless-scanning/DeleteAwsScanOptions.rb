2eb89a2ae4305f7aa2a38c9787a990debe4f0025
ci.datadog-api-spec <packages@datadoghq.com>
2025-03-03 16:05:02 +0000

    Regenerate client from commit c17ef1e9 of spec repo

---
+++ examples/v2/agentless-scanning/DeleteAwsScanOptions.rb
@@ -0,0 +1,5 @@
+# Delete AWS Scan Options returns "No Content" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::AgentlessScanningAPI.new
+api_instance.delete_aws_scan_options("account_id")
--- lib/datadog_api_client/v2/models/aws_scan_options_update_data.rb
+++ lib/datadog_api_client/v2/models/aws_scan_options_update_data.rb
@@ -17,12 +17,18 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # Response object that includes the scan options of an AWS account.
-  class AwsScanOptionsResponse
+  # Object for the scan options of a single AWS account.
+  class AwsScanOptionsUpdateData
     include BaseGenericModel
 
-    # Single AWS Scan Options entry.
-    attr_accessor :data
+    # Attributes for the AWS scan options to update.
+    attr_reader :attributes
+
+    # The ID of the AWS account.
+    attr_accessor :id
+
+    # The type of the resource. The value should always be `aws_scan_options`.
+    attr_reader :type
 
     attr_accessor :additional_properties
 
@@ -30,7 +36,9 @@ module DatadogAPIClient::V2
     # @!visibility private
     def self.attribute_map
       {
-        :'data' => :'data'
+        :'attributes' => :'attributes',
+        :'id' => :'id',
+        :'type' => :'type'
       }
     end
 
@@ -38,7 +46,9 @@ module DatadogAPIClient::V2
     # @!visibility private
     def self.openapi_types
       {
-        :'data' => :'AwsScanOptionsData'
+        :'attributes' => :'AwsScanOptionsUpdateAttributes',
+        :'id' => :'String',
+        :'type' => :'AwsScanOptionsType'
       }
     end
 
@@ -47,7 +57,7 @@ module DatadogAPIClient::V2
     # @!visibility private
     def initialize(attributes = {})
       if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::AwsScanOptionsResponse` initialize method"
+        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V2::AwsScanOptionsUpdateData` initialize method"
       end
 
       self.additional_properties = {}
@@ -60,9 +70,46 @@ module DatadogAPIClient::V2
         end
       }
 
-      if attributes.key?(:'data')
-        self.data = attributes[:'data']
+      if attributes.key?(:'attributes')
+        self.attributes = attributes[:'attributes']
       end
+
+      if attributes.key?(:'id')
+        self.id = attributes[:'id']
+      end
+
+      if attributes.key?(:'type')
+        self.type = attributes[:'type']
+      end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      return false if @attributes.nil?
+      return false if @type.nil?
+      true
+    end
+
+    # Custom attribute writer method with validation
+    # @param attributes [Object] Object to be assigned
+    # @!visibility private
+    def attributes=(attributes)
+      if attributes.nil?
+        fail ArgumentError, 'invalid value for "attributes", attributes cannot be nil.'
+      end
+      @attributes = attributes
+    end
+
+    # Custom attribute writer method with validation
+    # @param type [Object] Object to be assigned
+    # @!visibility private
+    def type=(type)
+      if type.nil?
+        fail ArgumentError, 'invalid value for "type", type cannot be nil.'
+      end
+      @type = type
     end
 
     # Returns the object in the form of hash, with additionalProperties support.
@@ -91,7 +138,9 @@ module DatadogAPIClient::V2
     def ==(o)
       return true if self.equal?(o)
       self.class == o.class &&
-          data == o.data &&
+          attributes == o.attributes &&
+          id == o.id &&
+          type == o.type &&
           additional_properties == o.additional_properties
     end
 
@@ -99,7 +148,7 @@ module DatadogAPIClient::V2
     # @return [Integer] Hash code
     # @!visibility private
     def hash
-      [data, additional_properties].hash
+      [attributes, id, type, additional_properties].hash
     end
   end
 end
