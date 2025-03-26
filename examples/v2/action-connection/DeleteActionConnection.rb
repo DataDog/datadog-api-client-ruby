24e133c9f9e81fa350300393add14b77975cc784
ci.datadog-api-spec <packages@datadoghq.com>
2025-03-26 20:30:32 +0000

    Regenerate client from commit 61cc2d5e of spec repo

--- examples/v2/action-connection/DeleteActionConnection.rb
+++ examples/v2/action-connection/DeleteActionConnection.rb
@@ -2,4 +2,7 @@
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V2::ActionConnectionAPI.new
-api_instance.delete_action_connection("connection_id")
+
+# there is a valid "action_connection" in the system
+ACTION_CONNECTION_DATA_ID = ENV["ACTION_CONNECTION_DATA_ID"]
+api_instance.delete_action_connection(ACTION_CONNECTION_DATA_ID)
