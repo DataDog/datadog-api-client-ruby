b3566486274442b32334f926b94303fb1a9a0b12
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-06 16:07:06 +0000

    Regenerate client from commit fe3756c4 of spec repo

--- examples/v2/security-monitoring/GetHistoricalJob.rb
+++ examples/v2/security-monitoring/GetHistoricalJob.rb
@@ -1,10 +1,6 @@
 # Get a job's details returns "OK" response
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.get_historical_job".to_sym] = true
-  config.unstable_operations["v2.run_historical_job".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 
 # there is a valid "historical_job" in the system
--- examples/v2/security-monitoring/RunHistoricalJob.rb
+++ examples/v2/security-monitoring/RunHistoricalJob.rb
@@ -1,9 +1,6 @@
 # Run a historical job returns "Status created" response
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.run_historical_job".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 
 body = DatadogAPIClient::V2::RunHistoricalJobRequest.new({
