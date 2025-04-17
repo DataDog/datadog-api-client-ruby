e99121e797d04e36f211f3dd4928034115aa4567
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-17 13:26:43 +0000

    Regenerate client from commit 12ab5180 of spec repo

--- lib/datadog_api_client/v2/models/layer.rb
+++ lib/datadog_api_client/v2/models/layer.rb
@@ -31,7 +31,7 @@ module DatadogAPIClient::V2
     attr_accessor :relationships
 
     # Layers resource type.
-    attr_accessor :type
+    attr_reader :type
 
     attr_accessor :additional_properties
 
@@ -90,6 +90,24 @@ module DatadogAPIClient::V2
       if attributes.key?(:'type')
         self.type = attributes[:'type']
       end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      return false if @type.nil?
+      true
+    end
+
+    # Custom attribute writer method with validation
+    # @param type [Object] Object to be assigned
+    # @!visibility private
+    def type=(type)
+      if type.nil?
+        fail ArgumentError, 'invalid value for "type", type cannot be nil.'
+      end
+      @type = type
     end
 
     # Returns the object in the form of hash, with additionalProperties support.
--- lib/datadog_api_client/v2/models/schedule_create_request_data_relationships_teams_data_items.rb
+++ lib/datadog_api_client/v2/models/schedule_create_request_data_relationships_teams_data_items.rb
@@ -23,10 +23,10 @@ module DatadogAPIClient::V2
     include BaseGenericModel
 
     # A unique identifier for the team.
-    attr_accessor :id
+    attr_reader :id
 
     # Teams resource type.
-    attr_accessor :type
+    attr_reader :type
 
     attr_accessor :additional_properties
 
@@ -73,6 +73,35 @@ module DatadogAPIClient::V2
       if attributes.key?(:'type')
         self.type = attributes[:'type']
       end
+    end
+
+    # Check to see if the all the properties in the model are valid
+    # @return true if the model is valid
+    # @!visibility private
+    def valid?
+      return false if @id.nil?
+      return false if @type.nil?
+      true
+    end
+
+    # Custom attribute writer method with validation
+    # @param id [Object] Object to be assigned
+    # @!visibility private
+    def id=(id)
+      if id.nil?
+        fail ArgumentError, 'invalid value for "id", id cannot be nil.'
+      end
+      @id = id
+    end
+
+    # Custom attribute writer method with validation
+    # @param type [Object] Object to be assigned
+    # @!visibility private
+    def type=(type)
+      if type.nil?
+        fail ArgumentError, 'invalid value for "type", type cannot be nil.'
+      end
+      @type = type
     end
 
     # Returns the object in the form of hash, with additionalProperties support.
