167cdb423926b92570ba2f36a02367347fce19e1
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-29 16:13:29 +0000

    Regenerate client from commit 22937387 of spec repo

---
+++ examples/v2/security-monitoring/DeleteCustomFramework.rb
@@ -0,0 +1,5 @@
+# Delete a custom framework returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
+p api_instance.delete_custom_framework("create-framework-new", "10")
