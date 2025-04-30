f1488aa8bcce654074afd89311e43e876bcda3cf
ci.datadog-api-spec <packages@datadoghq.com>
2025-04-30 13:02:14 +0000

    Regenerate client from commit ca363d75 of spec repo

---
+++ examples/v2/cloud-cost-management/UpsertBudget.rb
@@ -0,0 +1,35 @@
+# Create or update a budget returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::CloudCostManagementAPI.new
+
+body = DatadogAPIClient::V2::BudgetWithEntries.new({
+  data: DatadogAPIClient::V2::BudgetWithEntriesData.new({
+    attributes: DatadogAPIClient::V2::BudgetAttributes.new({
+      created_at: 1738258683590,
+      created_by: "00000000-0a0a-0a0a-aaa0-00000000000a",
+      end_month: 202502,
+      entries: [
+        DatadogAPIClient::V2::BudgetEntry.new({
+          amount: 500,
+          month: 202501,
+          tag_filters: [
+            DatadogAPIClient::V2::TagFilter.new({
+              tag_key: "service",
+              tag_value: "ec2",
+            }),
+          ],
+        }),
+      ],
+      metrics_query: "aws.cost.amortized{service:ec2} by {service}",
+      name: "my budget",
+      org_id: 123,
+      start_month: 202501,
+      total_amount: 1000,
+      updated_at: 1738258683590,
+      updated_by: "00000000-0a0a-0a0a-aaa0-00000000000a",
+    }),
+    id: "00000000-0a0a-0a0a-aaa0-00000000000a",
+  }),
+})
+p api_instance.upsert_budget(body)
