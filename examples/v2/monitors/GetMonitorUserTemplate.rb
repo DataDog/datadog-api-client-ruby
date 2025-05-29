6e874f81b77c0fcb5747649b13ac781d3f0fd0b1
ci.datadog-api-spec <packages@datadoghq.com>
2025-05-29 21:27:32 +0000

    Regenerate client from commit 26af43bc of spec repo

---
+++ examples/v2/monitors/GetMonitorUserTemplate.rb
@@ -0,0 +1,8 @@
+# Get a monitor user template returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::MonitorsAPI.new
+
+# there is a valid "monitor_user_template" in the system
+MONITOR_USER_TEMPLATE_DATA_ID = ENV["MONITOR_USER_TEMPLATE_DATA_ID"]
+p api_instance.get_monitor_user_template(MONITOR_USER_TEMPLATE_DATA_ID)
