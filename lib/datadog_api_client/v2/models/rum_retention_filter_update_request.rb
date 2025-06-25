ef24c1d391118789de65fcb9c6dea65e476596ff
ci.datadog-api-spec <packages@datadoghq.com>
2025-06-25 18:49:49 +0000

    Regenerate client from commit 1a1ecd3d of spec repo

--- lib/datadog_api_client/v2/models/rum_retention_filter_update_request.rb
+++ lib/datadog_api_client/v2/models/rum_retention_filter_update_request.rb
@@ -17,7 +17,7 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # The RUM retention filter body to update.
+  # The RUM retention filter body to update
   class RumRetentionFilterUpdateRequest
     include BaseGenericModel
 
