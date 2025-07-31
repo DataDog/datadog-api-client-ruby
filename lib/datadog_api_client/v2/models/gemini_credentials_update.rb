87b1e59cb6f7d383445776d10a3f45fe449405ba
ci.datadog-api-spec <packages@datadoghq.com>
2025-07-31 15:19:44 +0000

    Regenerate client from commit 356ca3c of spec repo

--- lib/datadog_api_client/v2/models/gemini_credentials_update.rb
+++ lib/datadog_api_client/v2/models/gemini_credentials_update.rb
@@ -17,8 +17,8 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # The definition of `ActionConnectionIntegration` object.
-  module ActionConnectionIntegration
+  # The definition of the `GeminiCredentialsUpdate` object.
+  module GeminiCredentialsUpdate
     class << self
       include BaseOneOfModel
       include BaseOneOfModelNoDiscriminator
@@ -26,30 +26,7 @@ module DatadogAPIClient::V2
       # List of class defined in oneOf (OpenAPI v3)
       def openapi_one_of
         [
-          :'AWSIntegration',
-          :'AnthropicIntegration',
-          :'AsanaIntegration',
-          :'AzureIntegration',
-          :'CircleCIIntegration',
-          :'ClickupIntegration',
-          :'CloudflareIntegration',
-          :'ConfigCatIntegration',
-          :'DatadogIntegration',
-          :'FastlyIntegration',
-          :'FreshserviceIntegration',
-          :'GCPIntegration',
-          :'GeminiIntegration',
-          :'GitlabIntegration',
-          :'GreyNoiseIntegration',
-          :'HTTPIntegration',
-          :'LaunchDarklyIntegration',
-          :'NotionIntegration',
-          :'OktaIntegration',
-          :'OpenAIIntegration',
-          :'ServiceNowIntegration',
-          :'SplitIntegration',
-          :'StatsigIntegration',
-          :'VirusTotalIntegration'
+          :'GeminiAPIKeyUpdate'
         ]
       end
       # Builds the object
