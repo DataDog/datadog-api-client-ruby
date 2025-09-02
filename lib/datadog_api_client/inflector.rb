cef300d42aea64a4b065d46a6d6415814dd3ad0a
ci.datadog-api-spec <packages@datadoghq.com>
2025-09-02 15:24:28 +0000

    Regenerate client from commit 1e79ef0 of spec repo

--- lib/datadog_api_client/inflector.rb
+++ lib/datadog_api_client/inflector.rb
@@ -1319,6 +1319,7 @@ module DatadogAPIClient
           "v2.ci_app_create_pipeline_event_request_attributes" => "CIAppCreatePipelineEventRequestAttributes",
           "v2.ci_app_create_pipeline_event_request_attributes_resource" => "CIAppCreatePipelineEventRequestAttributesResource",
           "v2.ci_app_create_pipeline_event_request_data" => "CIAppCreatePipelineEventRequestData",
+          "v2.ci_app_create_pipeline_event_request_data_single_or_array" => "CIAppCreatePipelineEventRequestDataSingleOrArray",
           "v2.ci_app_create_pipeline_event_request_data_type" => "CIAppCreatePipelineEventRequestDataType",
           "v2.ci_app_event_attributes" => "CIAppEventAttributes",
           "v2.ci_app_git_info" => "CIAppGitInfo",
