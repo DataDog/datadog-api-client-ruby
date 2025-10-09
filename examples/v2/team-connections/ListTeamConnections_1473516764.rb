c7935826e2468164789192d895f71d443367736d
ci.datadog-api-spec <packages@datadoghq.com>
2025-10-09 22:03:00 +0000

    Regenerate client from commit dfedc11 of spec repo

---
+++ examples/v2/team-connections/ListTeamConnections_1473516764.rb
@@ -0,0 +1,14 @@
+# List team connections with filters returns "OK" response
+
+require "datadog_api_client"
+DatadogAPIClient.configure do |config|
+  config.unstable_operations["v2.list_team_connections".to_sym] = true
+end
+api_instance = DatadogAPIClient::V2::TeamConnectionsAPI.new
+opts = {
+  filter_sources: [
+    "github",
+  ],
+  page_size: 10,
+}
+p api_instance.list_team_connections(opts)
