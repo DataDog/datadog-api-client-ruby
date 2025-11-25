d73f521395ded6680fc1505a3bc65d53e14d15b8
ci.datadog-api-spec <packages@datadoghq.com>
2025-11-25 17:45:00 +0000

    Regenerate client from commit 479ca66 of spec repo

---
+++ examples/v2/on-call/GetScheduleOnCallUser_3672445524.rb
@@ -0,0 +1,8 @@
+# Get the schedule on-call user returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::OnCallAPI.new
+
+# there is a valid "schedule" in the system
+SCHEDULE_DATA_ID = ENV["SCHEDULE_DATA_ID"]
+p api_instance.get_schedule_on_call_user(SCHEDULE_DATA_ID)
