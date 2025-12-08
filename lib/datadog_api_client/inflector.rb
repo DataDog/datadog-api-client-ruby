b4181727f2cd07c5e0b8b47dc090d92c5d2efeb8
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-08 14:20:29 +0000

    Regenerate client from commit ec57a65 of spec repo

--- lib/datadog_api_client/inflector.rb
+++ lib/datadog_api_client/inflector.rb
@@ -168,6 +168,8 @@ module DatadogAPIClient
           "v1.heat_map_widget_definition" => "HeatMapWidgetDefinition",
           "v1.heat_map_widget_definition_type" => "HeatMapWidgetDefinitionType",
           "v1.heat_map_widget_request" => "HeatMapWidgetRequest",
+          "v1.heat_map_widget_x_axis" => "HeatMapWidgetXAxis",
+          "v1.heat_map_widget_y_axis" => "HeatMapWidgetYAxis",
           "v1.host" => "Host",
           "v1.host_list_response" => "HostListResponse",
           "v1.host_map_request" => "HostMapRequest",
