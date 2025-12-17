cf17628706933b8d2bbd4ad8cc2de02be73a430f
ci.datadog-api-spec <packages@datadoghq.com>
2025-12-17 17:25:43 +0000

    Regenerate client from commit 9477bc4 of spec repo

--- examples/v2/teams/SyncTeams.rb
+++ examples/v2/teams/SyncTeams.rb
@@ -1,9 +1,6 @@
 # Link Teams with GitHub Teams returns "OK" response
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.sync_teams".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::TeamsAPI.new
 
 body = DatadogAPIClient::V2::TeamSyncRequest.new({
