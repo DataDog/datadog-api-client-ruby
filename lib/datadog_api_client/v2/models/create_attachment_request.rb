3ebac13b22a03cecc4cef671917a0a004f54ea82
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-07 15:39:21 +0000

    Regenerate client from commit b3af4b5 of spec repo

--- lib/datadog_api_client/v2/models/create_attachment_request.rb
+++ lib/datadog_api_client/v2/models/create_attachment_request.rb
@@ -17,11 +17,11 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V2
-  # 
+  # Create request for an attachment.
   class CreateAttachmentRequest
     include BaseGenericModel
 
-    #
+    # Attachment data for a create request.
     attr_accessor :data
 
     attr_accessor :additional_properties
