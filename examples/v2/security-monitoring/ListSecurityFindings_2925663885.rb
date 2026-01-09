5ba8f69019c0b5e809482cceef5319d8bb4425d4
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-09 20:05:29 +0000

    Regenerate client from commit 221df34 of spec repo

--- examples/v2/security-monitoring/ListSecurityFindings_2925663885.rb
+++ examples/v2/security-monitoring/ListSecurityFindings_2925663885.rb
@@ -1,9 +1,6 @@
 # List security findings returns "OK" response with pagination
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.list_security_findings".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 opts = {
   page_limit: 5,
--- examples/v2/security-monitoring/SearchSecurityFindings_3678541639.rb
+++ examples/v2/security-monitoring/SearchSecurityFindings_3678541639.rb
@@ -1,9 +1,6 @@
 # Search security findings returns "OK" response with pagination
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.search_security_findings".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 
 body = DatadogAPIClient::V2::SecurityFindingsSearchRequest.new({
