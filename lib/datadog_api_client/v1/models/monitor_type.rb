ccac7ac1ed842b8ae392f0108c3f6566b7c3d6d6
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-20 21:30:28 +0000

    Regenerate client from commit 5db4165 of spec repo

--- lib/datadog_api_client/v1/models/monitor_type.rb
+++ lib/datadog_api_client/v1/models/monitor_type.rb
@@ -41,5 +41,6 @@ module DatadogAPIClient::V1
     NETWORK_PERFORMANCE_ALERT = "network-performance alert".freeze
     COST_ALERT = "cost alert".freeze
     DATA_QUALITY_ALERT = "data-quality alert".freeze
+    NETWORK_PATH_ALERT = "network-path alert".freeze
   end
 end
