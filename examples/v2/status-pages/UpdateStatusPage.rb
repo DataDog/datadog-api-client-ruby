d533ff577fbfa92fc3ade74de685e2280598eeb1
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-21 17:59:55 +0000

    Regenerate client from commit 85d1ad4 of spec repo

---
+++ examples/v2/status-pages/UpdateStatusPage.rb
@@ -0,0 +1,18 @@
+# Update status page returns "OK" response
+
+require "datadog_api_client"
+api_instance = DatadogAPIClient::V2::StatusPagesAPI.new
+
+# there is a valid "status_page" in the system
+STATUS_PAGE_DATA_ID = ENV["STATUS_PAGE_DATA_ID"]
+
+body = DatadogAPIClient::V2::PatchStatusPageRequest.new({
+  data: DatadogAPIClient::V2::PatchStatusPageRequestData.new({
+    attributes: DatadogAPIClient::V2::PatchStatusPageRequestDataAttributes.new({
+      name: "A Status Page in US1",
+    }),
+    id: STATUS_PAGE_DATA_ID,
+    type: DatadogAPIClient::V2::StatusPageDataType::STATUS_PAGES,
+  }),
+})
+p api_instance.update_status_page(STATUS_PAGE_DATA_ID, body)
