f86da8925fa3399d9f1592472fc2f78b882b0a03
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-22 15:26:28 +0000

    Regenerate client from commit ab01d4d of spec repo

--- examples/v2/security-monitoring/CreateJiraIssues_829823123.rb
+++ examples/v2/security-monitoring/CreateJiraIssues_829823123.rb
@@ -1,4 +1,4 @@
-# Create Jira issues for security findings returns "Created" response
+# Create Jira issue for security findings returns "Created" response
 
 require "datadog_api_client"
 api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
@@ -14,30 +14,11 @@ body = DatadogAPIClient::V2::CreateJiraIssueRequestArray.new({
         findings: DatadogAPIClient::V2::Findings.new({
           data: [
             DatadogAPIClient::V2::FindingData.new({
-              id: "eWswLWJsdC1hZm5-aS0wMjRlYTgwMzVkZTU1MGIwYQ==",
+              id: "a3ZoLXNjbS14eXV-aS0wNWY5MGYwMGE4NDg2ODdlOA==",
               type: DatadogAPIClient::V2::FindingDataType::FINDINGS,
             }),
-          ],
-        }),
-        project: DatadogAPIClient::V2::CaseManagementProject.new({
-          data: DatadogAPIClient::V2::CaseManagementProjectData.new({
-            id: "959a6f71-bac8-4027-b1d3-2264f569296f",
-            type: DatadogAPIClient::V2::CaseManagementProjectDataType::PROJECTS,
-          }),
-        }),
-      }),
-      type: DatadogAPIClient::V2::JiraIssuesDataType::JIRA_ISSUES,
-    }),
-    DatadogAPIClient::V2::CreateJiraIssueRequestData.new({
-      attributes: DatadogAPIClient::V2::CreateJiraIssueRequestDataAttributes.new({
-        title: "A title",
-        description: "A description",
-      }),
-      relationships: DatadogAPIClient::V2::CreateJiraIssueRequestDataRelationships.new({
-        findings: DatadogAPIClient::V2::Findings.new({
-          data: [
             DatadogAPIClient::V2::FindingData.new({
-              id: "a3ZoLXNjbS14eXV-aS0wNWY5MGYwMGE4NDg2ODdlOA==",
+              id: "eWswLWJsdC1hZm5-aS0wMjRlYTgwMzVkZTU1MGIwYQ==",
               type: DatadogAPIClient::V2::FindingDataType::FINDINGS,
             }),
           ],
