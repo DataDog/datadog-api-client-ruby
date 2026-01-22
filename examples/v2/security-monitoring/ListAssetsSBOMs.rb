313b3b01b3e49b8981e28bb6adc81e70249d7aaf
ci.datadog-api-spec <packages@datadoghq.com>
2026-01-22 13:54:03 +0000

    Regenerate client from commit 319e516 of spec repo

--- examples/v2/security-monitoring/ListAssetsSBOMs.rb
+++ examples/v2/security-monitoring/ListAssetsSBOMs.rb
@@ -1,9 +1,6 @@
 # List assets SBOMs returns "OK" response
 
 require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.list_assets_sbo_ms".to_sym] = true
-end
 api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 opts = {
   filter_package_name: "pandas",
