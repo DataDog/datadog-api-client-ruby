c26be5b7f0ac33483a6a5d98b25e7a14cb67c16e
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-04 13:35:49 +0000

    Regenerate client from commit b81db2d of spec repo

--- features/scenarios_model_mapping.rb
+++ features/scenarios_model_mapping.rb
@@ -1932,6 +1932,13 @@ ScenariosModelMappings = {
             "dashboard_list_id" => "Integer",
             "body" => "DashboardListUpdateItemsRequest",
     },
+    "v2.SearchDashboards" => {
+            "query" => "String",
+            "sort" => "String",
+            "include" => "String",
+            "page" => "Integer",
+            "limit" => "Integer",
+    },
     "v2.CreateDataset" => {
             "body" => "DatasetCreateRequest",
     },
@@ -2771,6 +2778,13 @@ ScenariosModelMappings = {
             "to" => "Integer",
             "group_by" => "String",
             "tags" => "String",
+            "limit" => "Integer",
+    },
+    "v2.SearchNotebooks" => {
+            "query" => "String",
+            "sort" => "String",
+            "include" => "String",
+            "page" => "Integer",
             "limit" => "Integer",
     },
     "v2.ListPipelines" => {
