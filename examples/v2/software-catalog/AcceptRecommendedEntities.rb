c9a67bdf3b3a174af699ff83fb41004999645d1a
ci.datadog-api-spec <packages@datadoghq.com>
2026-02-26 08:58:19 +0000

    Regenerate client from commit b2190fe of spec repo

---
+++ examples/v2/software-catalog/AcceptRecommendedEntities.rb
@@ -0,0 +1,78 @@
+# Accept recommended entities in bulk returns "Accepted" response
+
+require "datadog_api_client"
+DatadogAPIClient.configure do |config|
+  config.unstable_operations["v2.accept_recommended_entities".to_sym] = true
+end
+api_instance = DatadogAPIClient::V2::SoftwareCatalogAPI.new
+
+body = [
+  DatadogAPIClient::V2::RecommendedEntityWithSchema.new({
+    id: "123abc456def",
+    schema: DatadogAPIClient::V2::EntityV3Service.new({
+      api_version: DatadogAPIClient::V2::EntityV3APIVersion::V3,
+      datadog: DatadogAPIClient::V2::EntityV3ServiceDatadog.new({
+        code_locations: [
+          DatadogAPIClient::V2::EntityV3DatadogCodeLocationItem.new({
+            paths: [],
+          }),
+        ],
+        events: [
+          DatadogAPIClient::V2::EntityV3DatadogEventItem.new({}),
+        ],
+        logs: [
+          DatadogAPIClient::V2::EntityV3DatadogLogItem.new({}),
+        ],
+        performance_data: DatadogAPIClient::V2::EntityV3DatadogPerformance.new({
+          tags: [],
+        }),
+        pipelines: DatadogAPIClient::V2::EntityV3DatadogPipelines.new({
+          fingerprints: [],
+        }),
+      }),
+      integrations: DatadogAPIClient::V2::EntityV3Integrations.new({
+        opsgenie: DatadogAPIClient::V2::EntityV3DatadogIntegrationOpsgenie.new({
+          service_url: "https://www.opsgenie.com/service/shopping-cart",
+        }),
+        pagerduty: DatadogAPIClient::V2::EntityV3DatadogIntegrationPagerduty.new({
+          service_url: "https://www.pagerduty.com/service-directory/Pshopping-cart",
+        }),
+      }),
+      kind: DatadogAPIClient::V2::EntityV3ServiceKind::SERVICE,
+      metadata: DatadogAPIClient::V2::EntityV3Metadata.new({
+        additional_owners: [
+          DatadogAPIClient::V2::EntityV3MetadataAdditionalOwnersItems.new({
+            name: "",
+          }),
+        ],
+        contacts: [
+          DatadogAPIClient::V2::EntityV3MetadataContactsItems.new({
+            contact: "https://slack/",
+            type: "slack",
+          }),
+        ],
+        id: "4b163705-23c0-4573-b2fb-f6cea2163fcb",
+        inherit_from: "application:default/myapp",
+        links: [
+          DatadogAPIClient::V2::EntityV3MetadataLinksItems.new({
+            name: "mylink",
+            type: "link",
+            url: "https://mylink",
+          }),
+        ],
+        name: "myService",
+        namespace: "default",
+        tags: [
+          "this:tag",
+          "that:tag",
+        ],
+      }),
+      spec: DatadogAPIClient::V2::EntityV3ServiceSpec.new({
+        component_of: [],
+        depends_on: [],
+        languages: [],
+      }),
+    }),
+  }),
+]
+p api_instance.accept_recommended_entities(body)
