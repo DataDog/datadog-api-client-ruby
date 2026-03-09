deceb042b408fed949246f66aac7ab42b4196348
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-09 14:18:26 +0000

    Regenerate client from commit 11f6077 of spec repo

---
+++ examples/v2/aws-integration/CreateAWSAccountCCMConfig.rb
@@ -0,0 +1,27 @@
+# Create AWS CCM config returns "AWS CCM Config object" response
+
+require "datadog_api_client"
+DatadogAPIClient.configure do |config|
+  config.unstable_operations["v2.create_aws_account_ccm_config".to_sym] = true
+end
+api_instance = DatadogAPIClient::V2::AWSIntegrationAPI.new
+
+body = DatadogAPIClient::V2::AWSCcmConfigRequest.new({
+  data: DatadogAPIClient::V2::AWSCcmConfigRequestData.new({
+    attributes: DatadogAPIClient::V2::AWSCcmConfigRequestAttributes.new({
+      ccm_config: DatadogAPIClient::V2::AWSCcmConfig.new({
+        data_export_configs: [
+          DatadogAPIClient::V2::DataExportConfig.new({
+            bucket_name: "billing",
+            bucket_region: "us-east-1",
+            report_name: "cost-and-usage-report",
+            report_prefix: "reports",
+            report_type: "CUR2.0",
+          }),
+        ],
+      }),
+    }),
+    type: DatadogAPIClient::V2::AWSCcmConfigType::CCM_CONFIG,
+  }),
+})
+p api_instance.create_aws_account_ccm_config("aws_account_config_id", body)
