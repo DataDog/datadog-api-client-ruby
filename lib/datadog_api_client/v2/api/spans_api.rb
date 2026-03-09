7554e119465f97fe63a48606c5e30c045712bb1f
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-09 15:50:06 +0000

    Regenerate client from commit 64b7d7f of spec repo

--- lib/datadog_api_client/v2/api/spans_api.rb
+++ lib/datadog_api_client/v2/api/spans_api.rb
@@ -178,7 +178,7 @@ module DatadogAPIClient::V2
         while true do
             response = list_spans(body, opts)
             @api_client.get_attribute_from_path(response, "data").each { |item| yield(item) }
-            if @api_client.get_attribute_from_path(response, "data").length < page_size
+            if @api_client.get_attribute_from_path(response, "data").length == 0
               break
             end
             @api_client.set_attribute_from_path(api_version, body, "data.attributes.page.cursor", SpansListRequestData, @api_client.get_attribute_from_path(response, "meta.page.after"))
@@ -284,7 +284,7 @@ module DatadogAPIClient::V2
         while true do
             response = list_spans_get(opts)
             @api_client.get_attribute_from_path(response, "data").each { |item| yield(item) }
-            if @api_client.get_attribute_from_path(response, "data").length < page_size
+            if @api_client.get_attribute_from_path(response, "data").length == 0
               break
             end
             @api_client.set_attribute_from_path(api_version, opts, "page_cursor", String, @api_client.get_attribute_from_path(response, "meta.page.after"))
