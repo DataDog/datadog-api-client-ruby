b42979a27be4a3880bda7fbd165fa658ef9619dc
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-13 03:51:49 +0000

    Regenerate client from commit 53a6366 of spec repo

--- lib/datadog_api_client/v1/models/slo_count_definition.rb
+++ lib/datadog_api_client/v1/models/slo_count_definition.rb
@@ -17,156 +17,49 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V1
-  # A count-based (metric) SLI specification, composed of three parts: the good events formula, the total events formula,
-  # and the underlying queries.
-  class SLOCountDefinition
-    include BaseGenericModel
+  # A count-based (metric) SLI specification, composed of three parts: the good events formula,
+  # the bad or total events formula, and the underlying queries.
+  # Exactly one of `total_events_formula` or `bad_events_formula` must be provided.
+  module SLOCountDefinition
+    class << self
+      include BaseOneOfModel
+      include BaseOneOfModelNoDiscriminator
 
-    # A formula that specifies how to combine the results of multiple queries.
-    attr_reader :good_events_formula
-
-    #
-    attr_reader :queries
-
-    # A formula that specifies how to combine the results of multiple queries.
-    attr_reader :total_events_formula
-
-    attr_accessor :additional_properties
-
-    # Attribute mapping from ruby-style variable name to JSON key.
-    # @!visibility private
-    def self.attribute_map
-      {
-        :'good_events_formula' => :'good_events_formula',
-        :'queries' => :'queries',
-        :'total_events_formula' => :'total_events_formula'
-      }
-    end
-
-    # Attribute type mapping.
-    # @!visibility private
-    def self.openapi_types
-      {
-        :'good_events_formula' => :'SLOFormula',
-        :'queries' => :'Array<SLODataSourceQueryDefinition>',
-        :'total_events_formula' => :'SLOFormula'
-      }
-    end
-
-    # Initializes the object
-    # @param attributes [Hash] Model attributes in the form of hash
-    # @!visibility private
-    def initialize(attributes = {})
-      if (!attributes.is_a?(Hash))
-        fail ArgumentError, "The input argument (attributes) must be a hash in `DatadogAPIClient::V1::SLOCountDefinition` initialize method"
+      # List of class defined in oneOf (OpenAPI v3)
+      def openapi_one_of
+        [
+          :'SLOCountDefinitionWithTotalEventsFormula',
+          :'SLOCountDefinitionWithBadEventsFormula'
+        ]
       end
+      # Builds the object
+      # @param data [Mixed] Data to be matched against the list of oneOf items
+      # @return [Object] Returns the model or the data itself
+      def build(data)
+        # Go through the list of oneOf items and attempt to identify the appropriate one.
+        # Note:
+        # - We do not attempt to check whether exactly one item matches.
+        # - No advanced validation of types in some cases (e.g. "x: { type: string }" will happily match { x: 123 })
+        #   due to the way the deserialization is made in the base_object template (it just casts without verifying).
+        # - TODO: scalar values are de facto behaving as if they were nullable.
+        # - TODO: logging when debugging is set.
+        openapi_one_of.each do |klass|
+          begin
+            next if klass == :AnyType # "nullable: true"
+            typed_data = find_and_cast_into_type(klass, data)
+            next if typed_data.respond_to?(:_unparsed) && typed_data._unparsed
+            return typed_data if typed_data
+          rescue # rescue all errors so we keep iterating even if the current item lookup raises
+          end
+        end
 
-      self.additional_properties = {}
-      # check to see if the attribute exists and convert string to symbol for hash key
-      attributes = attributes.each_with_object({}) { |(k, v), h|
-        if (!self.class.attribute_map.key?(k.to_sym))
-          self.additional_properties[k.to_sym] = v
+        if openapi_one_of.include?(:AnyType)
+          data
         else
-          h[k.to_sym] = v
-        end
-      }
-
-      if attributes.key?(:'good_events_formula')
-        self.good_events_formula = attributes[:'good_events_formula']
-      end
-
-      if attributes.key?(:'queries')
-        if (value = attributes[:'queries']).is_a?(Array)
-          self.queries = value
+          self._unparsed = true
+          DatadogAPIClient::UnparsedObject.new(data)
         end
       end
-
-      if attributes.key?(:'total_events_formula')
-        self.total_events_formula = attributes[:'total_events_formula']
-      end
-    end
-
-    # Check to see if the all the properties in the model are valid
-    # @return true if the model is valid
-    # @!visibility private
-    def valid?
-      return false if @good_events_formula.nil?
-      return false if @queries.nil?
-      return false if @queries.length < 1
-      return false if @total_events_formula.nil?
-      true
-    end
-
-    # Custom attribute writer method with validation
-    # @param good_events_formula [Object] Object to be assigned
-    # @!visibility private
-    def good_events_formula=(good_events_formula)
-      if good_events_formula.nil?
-        fail ArgumentError, 'invalid value for "good_events_formula", good_events_formula cannot be nil.'
-      end
-      @good_events_formula = good_events_formula
-    end
-
-    # Custom attribute writer method with validation
-    # @param queries [Object] Object to be assigned
-    # @!visibility private
-    def queries=(queries)
-      if queries.nil?
-        fail ArgumentError, 'invalid value for "queries", queries cannot be nil.'
-      end
-      if queries.length < 1
-        fail ArgumentError, 'invalid value for "queries", number of items must be greater than or equal to 1.'
-      end
-      @queries = queries
-    end
-
-    # Custom attribute writer method with validation
-    # @param total_events_formula [Object] Object to be assigned
-    # @!visibility private
-    def total_events_formula=(total_events_formula)
-      if total_events_formula.nil?
-        fail ArgumentError, 'invalid value for "total_events_formula", total_events_formula cannot be nil.'
-      end
-      @total_events_formula = total_events_formula
-    end
-
-    # Returns the object in the form of hash, with additionalProperties support.
-    # @return [Hash] Returns the object in the form of hash
-    # @!visibility private
-    def to_hash
-      hash = {}
-      self.class.attribute_map.each_pair do |attr, param|
-        value = self.send(attr)
-        if value.nil?
-          is_nullable = self.class.openapi_nullable.include?(attr)
-          next if !is_nullable || (is_nullable && !instance_variable_defined?(:"@#{attr}"))
-        end
-
-        hash[param] = _to_hash(value)
-      end
-      self.additional_properties.each_pair do |attr, value|
-        hash[attr] = value
-      end
-      hash
-    end
-
-    # Checks equality by comparing each attribute.
-    # @param o [Object] Object to be compared
-    # @!visibility private
-    def ==(o)
-      return true if self.equal?(o)
-      self.class == o.class &&
-          good_events_formula == o.good_events_formula &&
-          queries == o.queries &&
-          total_events_formula == o.total_events_formula &&
-          additional_properties == o.additional_properties
-    end
-
-    # Calculates hash code according to all attributes.
-    # @return [Integer] Hash code
-    # @!visibility private
-    def hash
-      [good_events_formula, queries, total_events_formula, additional_properties].hash
     end
   end
 end
--- lib/datadog_api_client/v1/models/slo_count_spec.rb
+++ lib/datadog_api_client/v1/models/slo_count_spec.rb
@@ -21,8 +21,9 @@ module DatadogAPIClient::V1
   class SLOCountSpec
     include BaseGenericModel
 
-    # A count-based (metric) SLI specification, composed of three parts: the good events formula, the total events formula,
-    # and the underlying queries.
+    # A count-based (metric) SLI specification, composed of three parts: the good events formula,
+    # the bad or total events formula, and the underlying queries.
+    # Exactly one of `total_events_formula` or `bad_events_formula` must be provided.
     attr_reader :count
 
     # Attribute mapping from ruby-style variable name to JSON key.
