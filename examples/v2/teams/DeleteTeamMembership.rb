c70abcb3bc2ebd6bfcbd092c48fd6495aee3710b
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-25 18:58:26 +0000

    Regenerate client from commit 646862c of spec repo

--- examples/v2/teams/DeleteTeamMembership.rb
+++ examples/v2/teams/DeleteTeamMembership.rb
@@ -5,4 +5,7 @@ api_instance = DatadogAPIClient::V2::TeamsAPI.new
 
 # there is a valid "dd_team" in the system
 DD_TEAM_DATA_ID = ENV["DD_TEAM_DATA_ID"]
-api_instance.delete_team_membership(DD_TEAM_DATA_ID, "user_id")
+
+# there is a valid "user" in the system
+USER_DATA_ID = ENV["USER_DATA_ID"]
+api_instance.delete_team_membership(DD_TEAM_DATA_ID, USER_DATA_ID)
