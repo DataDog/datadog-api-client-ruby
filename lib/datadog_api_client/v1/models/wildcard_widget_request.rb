66938243228943e9a5f25475c52dc2d11ed72af2
ci.datadog-api-spec <packages@datadoghq.com>
2026-03-26 09:10:27 +0000

    Regenerate client from commit b951844 of spec repo

--- lib/datadog_api_client/v1/models/wildcard_widget_request.rb
+++ lib/datadog_api_client/v1/models/wildcard_widget_request.rb
@@ -17,8 +17,8 @@ require 'date'
 require 'time'
 
 module DatadogAPIClient::V1
-  # [Definition of the widget](https://docs.datadoghq.com/dashboards/widgets/).
-  module WidgetDefinition
+  # Request object for the wildcard widget. Each variant represents a distinct data-fetching pattern: scalar formulas, timeseries formulas, list streams, and histograms.
+  module WildcardWidgetRequest
     class << self
       include BaseOneOfModel
       include BaseOneOfModelNoDiscriminator
@@ -26,43 +26,10 @@ module DatadogAPIClient::V1
       # List of class defined in oneOf (OpenAPI v3)
       def openapi_one_of
         [
-          :'AlertGraphWidgetDefinition',
-          :'AlertValueWidgetDefinition',
-          :'BarChartWidgetDefinition',
-          :'ChangeWidgetDefinition',
-          :'CheckStatusWidgetDefinition',
-          :'DistributionWidgetDefinition',
-          :'EventStreamWidgetDefinition',
-          :'EventTimelineWidgetDefinition',
-          :'FreeTextWidgetDefinition',
-          :'FunnelWidgetDefinition',
-          :'GeomapWidgetDefinition',
-          :'GroupWidgetDefinition',
-          :'HeatMapWidgetDefinition',
-          :'HostMapWidgetDefinition',
-          :'IFrameWidgetDefinition',
-          :'ImageWidgetDefinition',
-          :'ListStreamWidgetDefinition',
-          :'LogStreamWidgetDefinition',
-          :'MonitorSummaryWidgetDefinition',
-          :'NoteWidgetDefinition',
-          :'PowerpackWidgetDefinition',
-          :'QueryValueWidgetDefinition',
-          :'RunWorkflowWidgetDefinition',
-          :'SLOListWidgetDefinition',
-          :'SLOWidgetDefinition',
-          :'ScatterPlotWidgetDefinition',
-          :'SankeyWidgetDefinition',
-          :'ServiceMapWidgetDefinition',
-          :'ServiceSummaryWidgetDefinition',
-          :'SplitGraphWidgetDefinition',
-          :'SunburstWidgetDefinition',
-          :'TableWidgetDefinition',
-          :'TimeseriesWidgetDefinition',
-          :'ToplistWidgetDefinition',
-          :'TopologyMapWidgetDefinition',
-          :'TreeMapWidgetDefinition',
-          :'WildcardWidgetDefinition'
+          :'TreeMapWidgetRequest',
+          :'TimeseriesWidgetRequest',
+          :'ListStreamWidgetRequest',
+          :'DistributionWidgetRequest'
         ]
       end
       # Builds the object
