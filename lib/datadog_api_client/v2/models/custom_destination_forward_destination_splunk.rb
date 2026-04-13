fe89a166eab07f3a55fc3cb24c6d37d349536ef1
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-13 14:46:01 +0000

    Regenerate client from commit f4e1087 of spec repo

--- lib/datadog_api_client/v2/models/custom_destination_forward_destination_splunk.rb
+++ lib/datadog_api_client/v2/models/custom_destination_forward_destination_splunk.rb
@@ -28,6 +28,13 @@ module DatadogAPIClient::V2
     # Must have HTTPS scheme and forwarding back to Datadog is not allowed.
     attr_reader :endpoint
 
+    # The Splunk sourcetype for the events sent to this Splunk destination.
+    #
+    # If absent, the default sourcetype `_json` is used. If set to null, the `sourcetype`
+    # field is omitted from the Splunk HEC payload entirely. Otherwise, the provided string
+    # value is used as the sourcetype.
+    attr_accessor :sourcetype
+
     # Type of the Splunk HTTP Event Collector (HEC) destination.
     attr_reader :type
 
@@ -39,6 +46,7 @@ module DatadogAPIClient::V2
       {
         :'access_token' => :'access_token',
         :'endpoint' => :'endpoint',
+        :'sourcetype' => :'sourcetype',
         :'type' => :'type'
       }
     end
@@ -49,8 +57,17 @@ module DatadogAPIClient::V2
       {
         :'access_token' => :'String',
         :'endpoint' => :'String',
+        :'sourcetype' => :'String',
         :'type' => :'CustomDestinationForwardDestinationSplunkType'
       }
+    end
+
+    # List of attributes with nullable: true
+    # @!visibility private
+    def self.openapi_nullable
+      Set.new([
+        :'sourcetype',
+      ])
     end
 
     # Initializes the object
@@ -77,6 +94,10 @@ module DatadogAPIClient::V2
 
       if attributes.key?(:'endpoint')
         self.endpoint = attributes[:'endpoint']
+      end
+
+      if attributes.key?(:'sourcetype')
+        self.sourcetype = attributes[:'sourcetype']
       end
 
       if attributes.key?(:'type')
@@ -152,6 +173,7 @@ module DatadogAPIClient::V2
       self.class == o.class &&
           access_token == o.access_token &&
           endpoint == o.endpoint &&
+          sourcetype == o.sourcetype &&
           type == o.type &&
           additional_properties == o.additional_properties
     end
@@ -160,7 +182,7 @@ module DatadogAPIClient::V2
     # @return [Integer] Hash code
     # @!visibility private
     def hash
-      [access_token, endpoint, type, additional_properties].hash
+      [access_token, endpoint, sourcetype, type, additional_properties].hash
     end
   end
 end
