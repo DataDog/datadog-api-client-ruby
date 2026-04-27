4312acfb8e97edcd661b1aa10267f540ba51da40
ci.datadog-api-spec <packages@datadoghq.com>
2026-04-27 14:49:35 +0000

    Regenerate client from commit 16c48ca of spec repo

--- examples/v2/bits-ai-sre/GetInvestigation.rb
+++ examples/v2/bits-ai-sre/GetInvestigation.rb
@@ -1,8 +1,8 @@
-# Get a Bits AI investigation returns "OK" response
+# Get a Bits AI SRE investigation returns "OK" response
 
 require "datadog_api_client"
 DatadogAPIClient.configure do |config|
   config.unstable_operations["v2.get_investigation".to_sym] = true
 end
-api_instance = DatadogAPIClient::V2::BitsAIAPI.new
+api_instance = DatadogAPIClient::V2::BitsAISREAPI.new
 p api_instance.get_investigation("id")
--- examples/v2/bits-ai-sre/ListInvestigations.rb
+++ examples/v2/bits-ai-sre/ListInvestigations.rb
@@ -1,8 +1,8 @@
-# List Bits AI investigations returns "OK" response
+# List Bits AI SRE investigations returns "OK" response
 
 require "datadog_api_client"
 DatadogAPIClient.configure do |config|
   config.unstable_operations["v2.list_investigations".to_sym] = true
 end
-api_instance = DatadogAPIClient::V2::BitsAIAPI.new
+api_instance = DatadogAPIClient::V2::BitsAISREAPI.new
 p api_instance.list_investigations()
