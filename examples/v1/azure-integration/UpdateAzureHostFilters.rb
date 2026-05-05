111a94f217adaaf3212b9be539c5278c5668a310
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-05 18:17:34 +0000

    Regenerate client from commit e10a6b3 of spec repo

--- examples/v1/azure-integration/UpdateAzureHostFilters.rb
+++ examples/v1/azure-integration/UpdateAzureHostFilters.rb
@@ -26,7 +26,6 @@ body = DatadogAPIClient::V1::AzureAccount.new({
       namespace: "Microsoft.Compute",
     }),
   ],
-  secretless_auth_enabled: true,
   tenant_name: "testc44-1234-5678-9101-cc00736ftest",
   usage_metrics_enabled: true,
 })
--- lib/datadog_api_client/v2/api/cloud_cost_management_api.rb
+++ lib/datadog_api_client/v2/api/cloud_cost_management_api.rb
@@ -1640,6 +1640,66 @@ module DatadogAPIClient::V2
       return data, status_code, headers
     end
 
+    # List custom allocation rules status.
+    #
+    # @see #list_custom_allocation_rules_status_with_http_info
+    def list_custom_allocation_rules_status(opts = {})
+      data, _status_code, _headers = list_custom_allocation_rules_status_with_http_info(opts)
+      data
+    end
+
+    # List custom allocation rules status.
+    #
+    # List the processing status of all custom allocation rules. Returns only the ID and processing status for each rule.
+    #
+    # @param opts [Hash] the optional parameters
+    # @return [Array<(ArbitraryRuleStatusResponseArray, Integer, Hash)>] ArbitraryRuleStatusResponseArray data, response status code and response headers
+    def list_custom_allocation_rules_status_with_http_info(opts = {})
+
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: CloudCostManagementAPI.list_custom_allocation_rules_status ...'
+      end
+      # resource path
+      local_var_path = '/api/v2/cost/arbitrary_rule/status'
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'ArbitraryRuleStatusResponseArray'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth, :AuthZ]
+
+      new_options = opts.merge(
+        :operation => :list_custom_allocation_rules_status,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type,
+        :api_version => "V2"
+      )
+
+      data, status_code, headers = @api_client.call_api(Net::HTTP::Get, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: CloudCostManagementAPI#list_custom_allocation_rules_status\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
     # List Custom Costs files.
     #
     # @see #list_custom_costs_files_with_http_info
@@ -1769,6 +1829,66 @@ module DatadogAPIClient::V2
       data, status_code, headers = @api_client.call_api(Net::HTTP::Get, local_var_path, new_options)
       if @api_client.config.debugging
         @api_client.config.logger.debug "API called: CloudCostManagementAPI#list_tag_pipelines_rulesets\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
+
+    # List tag pipeline rulesets status.
+    #
+    # @see #list_tag_pipelines_rulesets_status_with_http_info
+    def list_tag_pipelines_rulesets_status(opts = {})
+      data, _status_code, _headers = list_tag_pipelines_rulesets_status_with_http_info(opts)
+      data
+    end
+
+    # List tag pipeline rulesets status.
+    #
+    # List the processing status of all tag pipeline rulesets. Returns only the ID and processing status for each ruleset.
+    #
+    # @param opts [Hash] the optional parameters
+    # @return [Array<(RulesetStatusRespArray, Integer, Hash)>] RulesetStatusRespArray data, response status code and response headers
+    def list_tag_pipelines_rulesets_status_with_http_info(opts = {})
+
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: CloudCostManagementAPI.list_tag_pipelines_rulesets_status ...'
+      end
+      # resource path
+      local_var_path = '/api/v2/tags/enrichment/status'
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'RulesetStatusRespArray'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth, :appKeyAuth, :AuthZ]
+
+      new_options = opts.merge(
+        :operation => :list_tag_pipelines_rulesets_status,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type,
+        :api_version => "V2"
+      )
+
+      data, status_code, headers = @api_client.call_api(Net::HTTP::Get, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: CloudCostManagementAPI#list_tag_pipelines_rulesets_status\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
       end
       return data, status_code, headers
     end
