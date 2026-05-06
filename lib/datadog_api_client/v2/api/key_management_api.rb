6d5ea313d92e08f5bc266f43fab08c2fa6e00e56
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-06 21:18:50 +0000

    Regenerate client from commit 71b45d2 of spec repo

--- lib/datadog_api_client/v2/api/key_management_api.rb
+++ lib/datadog_api_client/v2/api/key_management_api.rb
@@ -1354,5 +1354,71 @@ module DatadogAPIClient::V2
       end
       return data, status_code, headers
     end
+
+    # Validate API key.
+    #
+    # @see #validate_with_http_info
+    def validate(opts = {})
+      data, _status_code, _headers = validate_with_http_info(opts)
+      data
+    end
+
+    # Validate API key.
+    #
+    # Check if the API key is valid. Returns the organization UUID, API key ID, and associated scopes.
+    #
+    # @param opts [Hash] the optional parameters
+    # @return [Array<(ValidateV2Response, Integer, Hash)>] ValidateV2Response data, response status code and response headers
+    def validate_with_http_info(opts = {})
+      unstable_enabled = @api_client.config.unstable_operations["v2.validate".to_sym]
+      if unstable_enabled
+        @api_client.config.logger.warn format("Using unstable operation '%s'", "v2.validate")
+      else
+        raise DatadogAPIClient::APIError.new(message: format("Unstable operation '%s' is disabled", "v2.validate"))
+      end
+
+      if @api_client.config.debugging
+        @api_client.config.logger.debug 'Calling API: KeyManagementAPI.validate ...'
+      end
+      # resource path
+      local_var_path = '/api/v2/validate'
+
+      # query parameters
+      query_params = opts[:query_params] || {}
+
+      # header parameters
+      header_params = opts[:header_params] || {}
+      # HTTP header 'Accept' (if needed)
+      header_params['Accept'] = @api_client.select_header_accept(['application/json'])
+
+      # form parameters
+      form_params = opts[:form_params] || {}
+
+      # http body (model)
+      post_body = opts[:debug_body]
+
+      # return_type
+      return_type = opts[:debug_return_type] || 'ValidateV2Response'
+
+      # auth_names
+      auth_names = opts[:debug_auth_names] || [:apiKeyAuth]
+
+      new_options = opts.merge(
+        :operation => :validate,
+        :header_params => header_params,
+        :query_params => query_params,
+        :form_params => form_params,
+        :body => post_body,
+        :auth_names => auth_names,
+        :return_type => return_type,
+        :api_version => "V2"
+      )
+
+      data, status_code, headers = @api_client.call_api(Net::HTTP::Get, local_var_path, new_options)
+      if @api_client.config.debugging
+        @api_client.config.logger.debug "API called: KeyManagementAPI#validate\nData: #{data.inspect}\nStatus code: #{status_code}\nHeaders: #{headers}"
+      end
+      return data, status_code, headers
+    end
   end
 end
