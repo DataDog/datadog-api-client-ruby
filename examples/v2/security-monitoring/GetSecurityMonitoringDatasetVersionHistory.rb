0ed0e367fffae9197b9eba9ed06f58ea20065dca
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-26 19:53:07 +0000

    Regenerate client from commit 4404621 of spec repo

--- examples/v2/security-monitoring/GetSecurityMonitoringDatasetVersionHistory.rb
+++
@@ -1,8 +0,0 @@
-# Get the version history of a dataset returns "OK" response
-
-require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.get_security_monitoring_dataset_version_history".to_sym] = true
-end
-api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
-p api_instance.get_security_monitoring_dataset_version_history("123e4567-e89b-12d3-a456-426614174000")
