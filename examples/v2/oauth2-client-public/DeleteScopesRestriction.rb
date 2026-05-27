3b9fef184516550a69f2b1ff4136a5f8e07ac654
ci.datadog-api-spec <packages@datadoghq.com>
2026-05-27 21:02:22 +0000

    Regenerate client from commit 8292e96 of spec repo

--- examples/v2/oauth2-client-public/DeleteScopesRestriction.rb
+++
@@ -1,8 +0,0 @@
-# Delete an OAuth2 client scopes restriction returns "No Content" response
-
-require "datadog_api_client"
-DatadogAPIClient.configure do |config|
-  config.unstable_operations["v2.delete_scopes_restriction".to_sym] = true
-end
-api_instance = DatadogAPIClient::V2::OAuth2ClientPublicAPI.new
-api_instance.delete_scopes_restriction("fafa8e1c-36a5-11f0-a83d-da7ad0900001")
