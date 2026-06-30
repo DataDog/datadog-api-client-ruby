b2f77b437127f69c5c7bf35f97688e3f5d124217
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-30 07:15:32 +0000

    Regenerate client from commit 6a6a888 of spec repo

--- examples/v2/metrics/ListTagIndexingRulesForMetric.rb
+++ examples/v2/metrics/ListTagIndexingRulesForMetric.rb
@@ -1,5 +1,8 @@
 # List tag indexing rules for a metric returns "OK" response
 
 require "datadog_api_client"
+DatadogAPIClient.configure do |config|
+  config.unstable_operations["v2.list_tag_indexing_rules_for_metric".to_sym] = true
+end
 api_instance = DatadogAPIClient::V2::MetricsAPI.new
 p api_instance.list_tag_indexing_rules_for_metric("ExampleMetric")
