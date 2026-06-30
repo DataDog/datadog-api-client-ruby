17fd3009316729d934e42f94915e8357e7258ec2
ci.datadog-api-spec <packages@datadoghq.com>
2026-06-30 16:11:03 +0000

    Regenerate client from commit 76344ef of spec repo

--- lib/datadog_api_client/v1/models/usage_summary_date_org.rb
+++ lib/datadog_api_client/v1/models/usage_summary_date_org.rb
@@ -22,7 +22,7 @@ module DatadogAPIClient::V1
   # For SDK users only: all fields at this response level are accessible through the
   # `additionalProperties` map. Existing typed-field getters are unchanged. New billing
   # dimensions will not have typed-field getters. Use
-  # [Get available fields for usage summary](https://docs.datadoghq.com/api/latest/usage-metering/#get-usage-summary-available-fields)
+  # [Get available fields for usage summary](https://docs.datadoghq.com/api/latest/usage-metering/get-available-fields-for-usage-summary/)
   # to enumerate every available key.
   class UsageSummaryDateOrg
     include BaseGenericModel
