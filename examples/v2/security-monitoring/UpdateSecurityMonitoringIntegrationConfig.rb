90fb431bee6f3220cff169021f9b81fc1b3af8c7
ci.datadog-api-spec <packages@datadoghq.com>
2026-07-17 10:24:08 +0000

    Regenerate client from commit 93844a3 of spec repo

--- examples/v2/security-monitoring/UpdateSecurityMonitoringIntegrationConfig.rb
+++ examples/v2/security-monitoring/UpdateSecurityMonitoringIntegrationConfig.rb
@@ -8,12 +8,20 @@ api_instance = DatadogAPIClient::V2::SecurityMonitoringAPI.new
 
 body = DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigUpdateRequest.new({
   data: DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigUpdateData.new({
-    attributes: DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigUpdateAttributes.new({
+    attributes: DatadogAPIClient::V2::SecurityMonitoringGoogleWorkspaceIntegrationConfigUpdateAttributes.new({
       domain: "siem-test.com",
       enabled: true,
-      integration_type: DatadogAPIClient::V2::SecurityMonitoringIntegrationType::GOOGLE_WORKSPACE,
+      integration_type: DatadogAPIClient::V2::SecurityMonitoringIntegrationTypeGoogleWorkspace::GOOGLE_WORKSPACE,
       name: "My GWS Integration (renamed)",
-      secrets: DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigSecrets.new({}),
+      secrets: DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigGoogleWorkspaceSecrets.new({
+        admin_email: "admin@example.com",
+        service_account_json: DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigGoogleWorkspaceServiceAccount.new({
+          client_email: "svc@my-project.iam.gserviceaccount.com",
+          private_key: '-----BEGIN PRIVATE KEY-----\n...\n-----END PRIVATE KEY-----',
+          project_id: "my-project",
+          type: "service_account",
+        }),
+      }),
       settings: DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigSettings.new({}),
     }),
     type: DatadogAPIClient::V2::SecurityMonitoringIntegrationConfigResourceType::INTEGRATION_CONFIG,
