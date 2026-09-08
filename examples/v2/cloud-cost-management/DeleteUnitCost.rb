119e9c303e2e14e6c293a4de26c1794a3ffbf3c6
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-08 06:15:23 +0000

    Regenerate client from commit 02da321 of spec repo

---
+++ examples/v2/cloud-cost-management/DeleteUnitCost.rb
@@ -0,0 +1,8 @@
+# Delete a unit cost returns "No Content" response
+
+require "datadog_api_client"
+DatadogAPIClient.configure do |config|
+  config.unstable_operations["v2.delete_unit_cost".to_sym] = true
+end
+api_instance = DatadogAPIClient::V2::CloudCostManagementAPI.new
+api_instance.delete_unit_cost("9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d")
