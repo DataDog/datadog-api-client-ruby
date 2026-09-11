f4ae3eb70290259b366fe9fb91d70afe57c58c5c
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-11 14:53:49 +0000

    Regenerate client from commit eb76114 of spec repo

--- features/scenarios_model_mapping.rb
+++ features/scenarios_model_mapping.rb
@@ -4059,6 +4059,19 @@ ScenariosModelMappings = {
             "account_id" => "String",
             "body" => "ElasticCloudIntegrationAccountUpdateRequest",
     },
+    "v2.CreateSnowflakeIntegrationAccount" => {
+            "body" => "SnowflakeIntegrationAccountCreateRequest",
+    },
+    "v2.DeleteSnowflakeIntegrationAccount" => {
+            "account_id" => "String",
+    },
+    "v2.GetSnowflakeIntegrationAccount" => {
+            "account_id" => "String",
+    },
+    "v2.UpdateSnowflakeIntegrationAccount" => {
+            "account_id" => "String",
+            "body" => "SnowflakeIntegrationAccountUpdateRequest",
+    },
     "v2.CreateTwilioIntegrationAccount" => {
             "body" => "TwilioIntegrationAccountCreateRequest",
     },
