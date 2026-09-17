f443b6302a2d9e8e88cade7c304011e5f6435a55
ci.datadog-api-spec <packages@datadoghq.com>
2026-09-17 19:16:49 +0000

    Regenerate client from commit 49e5302 of spec repo

--- lib/datadog_api_client/v2/api/usage_metering_api.rb
+++ lib/datadog_api_client/v2/api/usage_metering_api.rb
@@ -1186,7 +1186,9 @@ module DatadogAPIClient::V2
 
     # List usage quotas.
     #
-    # Lists usage quotas for the caller's organization in a quota namespace. You can optionally include descendant organizations in the same datacenter as the caller. Requires the `billing_edit` permission.
+    # Lists usage quotas for the caller's organization in a quota namespace. You can optionally include
+    # descendant organizations in the same datacenter as the caller. Requires the `billing_read` or
+    # `billing_edit` permission.
     #
     # @param quota_namespace [String] The product-specific namespace whose usage quotas are being managed.
     # @param opts [Hash] the optional parameters
